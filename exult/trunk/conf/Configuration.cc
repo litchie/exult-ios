9b7bd9e83bd4ec63b4eca9145e36287f080961fb
dancer <dancer@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-03-12 08:22:44 +0000

    First cut of the XML-like-configuration-blob-class

---
+++ exult/trunk/conf/Configuration.cc
@@ -0,0 +1,57 @@
+
+
+#if __GNUG__ >= 2
+#  pragma implementation
+#endif
+
+
+#include "Configuration.h"
+
+#include <cstdio>
+
+
+
+Configuration::Configuration() :filename("")
+{}
+
+Configuration::Configuration(const char *s) : filename("")
+{
+	filename=s;
+	read_config_file(s);
+}
+
+Configuration::~Configuration()
+{}
+
+
+string	&Configuration::value(const char *key)
+{
+	string	s=key;
+	return xmltree.reference(s);
+}
+
+
+extern	void    xmlparse(string &s,size_t &pos,XMLnode *x);
+
+bool	Configuration::read_config_file(const char *n)
+{
+        char    buf[4096];
+        string  sbuf;
+
+	FILE	*fp=fopen(n,"r");
+	if(!fp)
+		return false;
+
+        while(fgets(buf,sizeof(buf),fp))
+                {
+                sbuf+=buf;
+                }
+
+        size_t  nn=1;
+        xmlparse(sbuf,nn,&xmltree);
+
+	fclose(fp);
+	return true;
+}
+
+
---
+++ exult/trunk/conf/XMLEntity.cc
@@ -0,0 +1,63 @@
+/*
+Copyright (C) 2000  Dancer A.L Vesperman
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+
+
+#if __GNUG__ >= 2
+#  pragma implementation
+#endif
+
+
+
+#include "XMLEntity.h"
+
+XMLEntity::XMLEntity() : id(""),content("") {}
+XMLEntity::~XMLEntity() {}
+
+
+XMLnode::XMLnode() {}
+XMLnode::~XMLnode() {}
+
+
+string	&XMLnode::reference(string &h)
+{
+	static string dummy("");
+	if(h.find('/')==string::npos)
+		{
+		// Must refer to me.
+		if(entity.id==h)
+			return entity.content;
+		else
+			return dummy;
+		}
+	// Otherwise we want to split the string at the first /
+	// then locate the branch to walk, and pass the rest
+	// down.
+
+	string k;
+	k=h.substr(h.find('/')+1);
+	string k2=k.substr(0,k.find('/'));
+	for(vector<XMLnode>::iterator it=nodelist.begin();
+		it!=nodelist.end();++it)
+		{
+		if(it->entity.id==k2)
+			return it->reference(k);
+		}
+
+	return dummy;
+}
