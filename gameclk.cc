4095f6c74b2628d2f3d81d3cb2f0e7fc56bc9788
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-03-30 19:28:18 +0000

    Untested palette changes

--- gameclk.cc
+++ gameclk.cc
@@ -1,4 +1,5 @@
-/**
+/**	-*-mode: Fundamental; tab-width: 8; -*-
+**
  **	Gameclk.cc - Keep track of time.
  **
  **	Written: 2/16/00 - JSF
@@ -7,6 +8,15 @@
 #include <iostream.h>			/* Debugging. */
 #include "gameclk.h"
 #include "gamewin.h"
+
+/*
+ *	Palette #'s in 'palettes.flx'.  Just need them here for now.
+	++++++Need to verify #'s.
+ */
+const int PALETTE_DAY = 0;
+const int PALETTE_DUSK = 1;
+const int PALETTE_NIGHT = 2;
+const int PALETTE_DAWN = 3;
 
 /*
  *	Animation.
@@ -20,17 +30,24 @@ void Game_clock::handle_event
 	{
 	if ((minute += 12) >= 60)	// 1 real minute = 12 game minutes.
 		{
+		Game_window *gwin = (Game_window *) udata;
 		minute -= 60;
 		if (++hour >= 24)
 			{
 			hour -= 24;
 			day++;
 			}
+		if (hour == 5)
+			gwin->set_palette(PALETTE_DAWN);
+		if (hour == 6)
+			gwin->set_palette(PALETTE_DAY);
+		if (hour == 19)
+			gwin->set_palette(PALETTE_DUSK);
+		if (hour == 21)
+			gwin->set_palette(PALETTE_NIGHT);
 		if (hour%3 == 0)	// New 3-hour period?
-			{		// Update NPC schedules.
-			Game_window *gwin = (Game_window *) udata;
+					// Update NPC schedules.
 			gwin->schedule_npcs(hour/3);
-			}
 		}
 	cout << "Clock updated to " << hour << ':' << minute << '\n';
 	curtime += 60*1000;		// Do it again in 60 seconds.
