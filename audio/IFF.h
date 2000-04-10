422119fc3bb1fc90ae4042e0bc050577792835b0
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2000-04-10 18:40:19 +0000

    changed #ifndef __Flex_h_ to #ifndef __IFF_h_

--- audio/IFF.h
+++ audio/IFF.h
@@ -16,8 +16,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifndef __Flex_h_
-#define __Flex_h_
+#ifndef __IFF_h_
+#define __IFF_h_
 
 #if !AUTOCONFIGURED
 #include "../autoconfig.h"
