b284126f289c6ae0916b66a11d29c43dddb1e20b
Tristan Tarrant <nadir@users.sourceforge.net>
2000-04-28 08:20:25 +0000

    2000-04-28  Tristan Tarrant  <tarrant_tristan@emc.com>
    	* autogen.sh: clear old generated files first
    	* configure.in: if I don't have kmid build without it anyway, don't
    	tell me to run configure with --disable-kmid

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# First of all clean up the generated crud
+rm -f configure config.log config.guess config.sub config.cache
+rm -f libtool ltmain.sh missing mkinstalldirs install-sh
+rm -f autoconfig.h.in
+rm -f config.status aclocal.m4
+rm -f `find . -name 'Makefile.in'`
+rm -f `find . -name 'Makefile'`
+
+# Regenerate everything
 libtoolize --force --copy
 aclocal
 autoheader
