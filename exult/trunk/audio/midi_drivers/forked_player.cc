8422f3a93d3af3febb86da0cc2c2d713122dfe6a
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-04-30 15:34:27 +0000

    config to *config

--- exult/trunk/audio/midi_drivers/forked_player.cc
+++ exult/trunk/audio/midi_drivers/forked_player.cc
@@ -30,7 +30,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../../fnames.h"
 
 #include "Configuration.h"
-extern	Configuration	config;
+extern	Configuration *config;
 
 static  void    playFJmidifile(const char *name)
 {
