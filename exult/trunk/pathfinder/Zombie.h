805b79bf53327abff8cb3c4c8c8e92e057bba586
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-05-05 00:49:51 +0000

    Pathfinding

--- exult/trunk/pathfinder/Zombie.h
+++ exult/trunk/pathfinder/Zombie.h
@@ -7,10 +7,10 @@
 
 class	Zombie: public virtual PathFinder
 	{
-	// Find a path from sx,sy to dx,dy
+	// Find a path from sx,sy,sz to dx,dy,dz
 	// Return 0 if no path can be traced.
 	// Return !0 if path found
-	virtual	int	NewPath(int sx,int sy,int dx,int dy,int (*tileclassifier)(int,int));
+	virtual	int	NewPath(int sx,int sy,int sz,int dx,int dy,int dz,int (*tileclassifier)(int,int,int));
 
 	// Retrieve the coordinates of the next step on the path
 	virtual	int	GetNextStep(int &nx,int &ny);
