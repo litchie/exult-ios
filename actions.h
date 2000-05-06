76cbcd09123f654ceeff5e29866b5ee1cc4b4847
Jeff Freedman <jsf@users.sourceforge.net>
2000-05-06 19:14:58 +0000

    Rewritten path stuff mostly working

--- actions.h
+++ actions.h
@@ -26,6 +26,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef INCL_ACTIONS
 #define INCL_ACTIONS	1
 
+#include "tiles.h"
+
 class Actor;
 class Tile_coord;
 class PathFinder;
@@ -39,20 +41,8 @@ public:
 					// Handle time event.
 	virtual int handle_event(Actor *actor) = 0;
 	virtual void stop(Actor *actor) = 0;	// Stop moving.
-	};
-
-/*
- *	Walk an actor towards a goal.
- */
-class Walking_actor_action : public Actor_action
-	{
-public:
-	virtual ~Walking_actor_action()
-		{  }
-					// Handle time event.
-	virtual int handle_event(Actor *actor);
-	virtual void stop(Actor *actor)// Stop moving.
-		{  }
+					// Set simple path to destination.
+	virtual Actor_action *walk_to_tile(Tile_coord src, Tile_coord dest);
 	};
 
 /*
@@ -69,6 +59,8 @@ public:
 					// Handle time event.
 	virtual int handle_event(Actor *actor);
 	virtual void stop(Actor *actor);// Stop moving.
+					// Set simple path to destination.
+	virtual Actor_action *walk_to_tile(Tile_coord src, Tile_coord dest);
 	};
 
 /*
