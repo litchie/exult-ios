11f41949438a052089fd1ba10d481c3fbb0993b0
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2000-05-10 17:25:59 +0000

    small fix to anims

--- objs.cc
+++ objs.cc
@@ -561,18 +561,19 @@ void Animated_object::handle_event
 		}
 	if (frames > 1)			// Going through frames?
 		{
+		int framenum;
 					// Experiment:  A non-flat shape?
 		if (get_shapenum() >= 0x96)
 			{		// Get next frame.
-			int framenum = get_framenum() + 1;
+			framenum = get_framenum() + 1;
 			if (framenum >= frames)	// End of cycle?
 				framenum = 0;
 			}
 		else			// Want flat shapes synchronized.
 			{		// Testing -WJP
-			int framenum = (curtime / 100) % frames;
+			framenum = (curtime / 100) % frames;
 			}
-			set_frame(framenum);	// Set new frame.
+		set_frame(framenum);	// Set new frame.
 		}
 	else
 		{
