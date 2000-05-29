50b447c1c140252d7797bb56e70f36579247e92e
Max Horn <max@quendi.de>
2000-05-29 17:00:32 +0000

    made some changes for MacOS support

--- usecode.h
+++ usecode.h
@@ -26,8 +26,14 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#ifdef MACOS
+#include <iostream>
+using namespace std;
+
+#else
 class istream;
 class ostream;
+#endif
 class Game_window;
 class Game_object;
 class Text_gump;
