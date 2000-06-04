c011609b48dec2d4476463efd492461b539b79bb
Jeff Freedman <jsf@users.sourceforge.net>
2000-06-04 18:52:42 +0000

    Working on scrolling

--- objs.h
+++ objs.h
@@ -54,6 +54,7 @@ const int tiles_per_chunk = 16;		// A chunk is 16x16 tiles.
 const int chunksize = 16 * 8;		// A chunk has 16 8x8 shapes.
 const int num_chunks = 12*16;		// Total # of chunks in each dir.
 const int chunks_per_schunk = 16;	// # chunks in each superchunk.
+const int tiles_per_schunk = 16*16;	// # tiles in each superchunk.
 					// Total # tiles in each dir.:
 const int num_tiles = tiles_per_chunk*num_chunks;
 
