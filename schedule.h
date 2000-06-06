718348fadb2184115d5b21c0c7a3255c23ad2b60
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-06-06 22:00:13 +0000

    Split out schedule classes

---
+++ schedule.h
@@ -0,0 +1,196 @@
+/**	-*-mode: Fundamental; tab-width: 8; -*-
+ **
+ **	Schedule.h - Schedules for characters.
+ **
+ **	Written: 6/6/2000 - JSF
+ **/
+
+/*
+Copyright (C) 1998  Jeffrey S. Freedman
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef INCL_SCHEDULE
+#define INCL_SCHEDULE	1
+
+#include "tiles.h"
+#include "vec.h"
+
+class Npc_actor;
+class Game_object;
+class Actor;
+class Rectangle;
+
+/*
+ *	A Schedule controls the NPC it is assigned to.
+ */
+class Schedule
+	{
+protected:
+	Npc_actor *npc;			// Who this controls.
+	Tile_coord blocked;		// Tile where actor was blocked.
+public:
+	Schedule(Npc_actor *n) : npc(n), blocked(-1, -1, -1)
+		{  }
+	void set_blocked(Tile_coord b)
+		{ blocked = b; }
+	enum Schedule_types {
+		combat = 0,	horiz_pace = 1,
+		vert_pace = 2,	talk = 3,
+		dance = 4,	eat = 5,
+		farm = 6,	tend_shop = 7,
+		miner = 8,	hound = 9,
+		stand = 10,	loiter = 11,
+		wander = 12,	blacksmith = 13,
+		sleep = 14,	wait = 15,
+		sit = 16,	graze = 17,
+		bake = 18,	sew = 19,
+		shy = 20,	lab = 21,
+		thief = 22,	waiter = 23,
+		special = 24,	kid_games = 25,
+		eat_at_inn = 26,duel = 27,
+		preach = 28,	patrol = 29,
+		desk_work = 30,	follow_avatar = 31,
+					// Our own:
+		walk_to_schedule = 32
+		};
+	virtual void now_what() = 0;	// Npc calls this when it's done
+					//   with its last task.
+	virtual void im_dormant()	// Npc calls this when it goes from
+		{  }			//   being active to dormant.
+	};
+
+/*
+ *	A schedule for pacing between two points:
+ */
+class Pace_schedule : public Schedule
+	{
+	Tile_coord p0;			// Point 0 tile coords.
+	Tile_coord p1;			// Point 1 tile coords.
+	char which;			// Which he's going to (0 or 1).
+public:
+	Pace_schedule(Npc_actor *n, Tile_coord pt0, Tile_coord pt1)
+		: Schedule(n), p0(pt0), p1(pt1), which(0)
+		{  }
+					// Create common schedules:
+	static Pace_schedule *create_horiz(Npc_actor *n);
+	static Pace_schedule *create_vert(Npc_actor *n);
+	virtual void now_what();	// Now what should NPC do?
+	};
+
+/*
+ *	A schedule for patrolling along 'path' objects.
+ */
+class Patrol_schedule : public Schedule
+	{
+	Vector paths;			// Each 'path' object.
+	int pathnum;			// # of next we're heading towards.
+public:
+	Patrol_schedule(Npc_actor *n)
+		: Schedule(n), pathnum(-1)
+		{  }
+	virtual void now_what();	// Now what should NPC do?
+	virtual ~Patrol_schedule();
+	};
+
+/*
+ *	Talk to avatar.
+ */
+class Talk_schedule : public Schedule
+	{
+	int phase;			// 0=walk to Av., 1=talk, 2=done.
+public:
+	Talk_schedule(Npc_actor *n) : Schedule(n), phase(0)
+		{  }
+	virtual void now_what();	// Now what should NPC do?
+	};
+
+/*
+ *	Loiter within a rectangle.
+ */
+class Loiter_schedule : public Schedule
+	{
+	Tile_coord center;		// Center of rectangle.
+	int dist;			// Distance in tiles to roam in each
+					//   dir.
+public:
+	Loiter_schedule(Npc_actor *n, int d = 12);
+	virtual void now_what();	// Now what should NPC do?
+	};
+
+/*
+ *	Sleep in a  bed.
+ */
+class Sleep_schedule : public Schedule
+	{
+public:
+	Sleep_schedule(Npc_actor *n);
+	virtual void now_what();	// Now what should NPC do?
+	};
+
+/*
+ *	Sit in a chair.
+ */
+class Sit_schedule : public Schedule
+	{
+	Game_object *chair;		// What to sit in.
+public:
+	Sit_schedule(Npc_actor *n, Game_object *ch = 0);
+	virtual void now_what();	// Now what should NPC do?
+	static void set_action(Actor *actor, Game_object *chairobj);
+	};
+
+/*
+ *	Walk to the destination for a new schedule.
+ */
+class Walk_to_schedule : public Schedule
+	{
+	Tile_coord dest;		// Where we're going.
+	int first_delay;		// Starting delay (1/1000's sec.)
+	int new_schedule;		// Schedule to set when we get there.
+	int retries;			// # failures at finding path.
+	int legs;			// # times restarted walk.
+					// Open door blocking NPC.
+	void open_door(Game_object *door);
+					// Set to walk off screen.
+	void walk_off_screen(Rectangle& screen, Tile_coord& goal);
+public:
+	Walk_to_schedule(Npc_actor *n, Tile_coord d, int new_sched);
+	virtual void now_what();	// Now what should NPC do?
+	virtual void im_dormant();	// Just went dormant.
+	};
+
+/*
+ *	An NPC schedule change:
+ */
+class Schedule_change
+	{
+	unsigned char time;		// Time*3hours when this takes effect.
+	unsigned char type;		// Schedule_type value.
+	unsigned char x, y;		// Location within superchunk.
+	unsigned char superchunk;	// 0-143.
+public:
+	Schedule_change() : time(0), type(0), x(0), y(0), superchunk(0)
+		{  }
+	void set(unsigned char *ent);	// Create from 5-byte entry.
+	int get_type() const
+		{ return type; }
+	int get_time() const
+		{ return time; }
+	Tile_coord get_pos() const;	// Get position chunk, tile.
+	};
+
+#endif
