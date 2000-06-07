36b2be072e0e7f21edddbaa76a098aa2ae6d7fc6
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-06-07 04:10:49 +0000

    Handling 'quantity' objects in drag.cc

--- gamewin.h
+++ gamewin.h
@@ -106,7 +106,6 @@ private:
 	Game_object *dragging;		// What's being dragged.
 	Gump_object *dragging_gump;
 	Gump_button *dragging_gump_button;
-	int dragging_quantity;		// Quantity to drag.
 					// Last mouse, paint positions:
 	int dragging_mousex, dragging_mousey, dragging_paintx, dragging_painty;
 	Rectangle dragging_rect;	// Rectangle to repaint.
