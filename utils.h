e755f3145a09b14d5c9c127f7a5c055a589e6699
Dancer Vesperman <dancer@users.sourceforge.net>
2000-06-18 22:42:57 +0000

    Fixed a small ios-declaration related compile-bug

--- utils.h
+++ utils.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef INCL_UTILS
 #define INCL_UTILS	1
 
-#include <iostream>
+#include <iosfwd>
 
 #include <exception>
 #include <string>
