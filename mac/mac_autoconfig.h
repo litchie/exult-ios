4218a9ae955ceb487494961fc6dcf9b193102d5b
Max Horn <max@quendi.de>
2000-06-28 21:39:35 +0000

    * mac support files

---
+++ mac/mac_autoconfig.h
@@ -0,0 +1,64 @@
+/* autoconfig.h.  Generated automatically by configure.  */
+/* autoconfig.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define as __inline if that's what the C compiler calls it.  */
+/* #undef inline */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if your <sys/time.h> declares struct tm.  */
+/* #undef TM_IN_SYS_TIME */
+
+/* Do we have a version of SDL that we can compile against? */
+#define HAVE_SDL 1
+
+/* Do we have a version of libkmidi that we can compile against? */
+/* #undef HAVE_LIBKMIDI */
+
+/* Do we have a version of freetype that we can compile against? */
+#define HAVE_FREETYPE 1
+
+      
+#define PACKAGE "exult"
+#define VERSION "0.26"
+
+/* Was debugging selected from configure? */
+#define DEBUG 1
+
+/* Multiple inclusion preventer. Is there a cleaner way to do this in autconf files? */
+#define AUTOCONFIGURED 1
+
+/* Do we appear to have a runnable copy of Timidity in our path? */
+/* #undef HAVE_TIMIDITY_BIN */
+
+/* Define if you have the <dirent.h> header file.  */
+/* #undef HAVE_DIRENT_H */
+
+/* Define if you have the <limits.h> header file.  */
+#define HAVE_LIMITS_H 1
+
+/* Define if you have the <ndir.h> header file.  */
+/* #undef HAVE_NDIR_H */
+
+/* Define if you have the <sys/dir.h> header file.  */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define if you have the <sys/ndir.h> header file.  */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define if you have the <sys/time.h> header file.  */
+/* #undef HAVE_SYS_TIME_H */
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if the C++ compiler supports BOOL */
+#define HAVE_BOOL 1
+
---
+++ mac/mac_prefix.h
@@ -0,0 +1,14 @@
+// this tells the code we compile on the macintosh.
+#define		MACOS	1
+
+
+// no autoconf on macintosh, thus "emulate" it
+#include	"mac_autoconfig.h"
+
+// missing defines:
+#define		RELNUM	30
+
+
+// missing functions:
+#define		isascii(x)	(!((x) & 0x80))
+char*		strdup( const char *x );
