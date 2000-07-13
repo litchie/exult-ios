e6ebc4090d0d74e900ad99717dae14c3d109428c
nadir <nadir@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-07-13 14:22:15 +0000

    Update the list of authors.

--- exult/trunk/titles.cc
+++ exult/trunk/titles.cc
@@ -58,8 +58,8 @@ void Titles::play_intro()
 		gwin->paint_shape(topx,topy,shapes.get_shape(0x11,0));
 		char *txt_msg[] = { "& Jeff Freedman, Dancer Vesperman,", 
 							"Willem Jan Palenstijn, Tristan Tarrant,", 
-							"Max Horn",
-							"Driven by the Exult game engine" };
+							"Max Horn, Coder Infidel",
+							"Driven by the Exult game engine V" VERSION };
 		for(int i=0; i<3; i++) {
 			gwin->paint_text(0, txt_msg[i], centerx-gwin->get_text_width(0, txt_msg[i])/2, centery+50+15*i);
 		}
