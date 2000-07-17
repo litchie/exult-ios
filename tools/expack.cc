e468c4a87dcdc68f2f8908895a6f19db2a48ca8a
Tristan Tarrant <nadir@users.sourceforge.net>
2000-07-17 15:48:14 +0000

    A bit better. Extracts single files or the entire archive. Still not very good or useful...

--- tools/expack.cc
+++ tools/expack.cc
@@ -5,41 +5,83 @@
 
 int main(int argc, char **argv)
 {
-  enum Arch_mode { LIST, EXTRACT } mode;
+  enum Arch_mode { NONE, LIST, EXTRACT, CAT, ADD } mode;
   char *fname;
   int index;
   
-  mode = LIST;
-
-  if(argc==2) {
-    fname = argv[1];
-
-    U7FileManager fm;
-    U7file *f = fm.get_file_object(fname);
-    int count = f->number_of_objects(0);
-    cout << "Archive: " << fname << endl;
-    cout << "Type: " << f->get_archive_type() << endl;
-    cout << "Size: " << count << endl;
-    cout << "-------------------------" << endl;
-    for(int i=0; i<count; i++) {
+  mode = NONE;
+  fname = 0;
+  
+  if(argc>2) {
+    fname = argv[2];
+    if((argv[1][0]=='-')&&(strlen(argv[1])==2)) {
+      switch(argv[1][1]) {
+      case 'l':
+	mode = LIST;
+	break;
+      case 'x':
+	mode = EXTRACT;
+	break;
+      case 'c':
+	mode = CAT;
+	break;
+      case 'a':
+	mode = ADD;
+	break;
+      default:
+	mode = NONE;
+	break;
+      }
+    }
+  }
+  
+  switch(mode) {
+  case LIST:
+    {
+      if(argc!=3)
+	break;
+      U7FileManager fm;
+      U7file *f = fm.get_file_object(fname);
+      int count = f->number_of_objects(0);
+      cout << "Archive: " << fname << endl;
+      cout << "Type: " << f->get_archive_type() << endl;
+      cout << "Size: " << count << endl;
+      cout << "-------------------------" << endl;
+      for(int i=0; i<count; i++) {
 	char *buf;
 	size_t len;
 	
 	f->retrieve(i, &buf, &len);
 	cout << (i+1) << "\t" << len << endl;
 	delete [] buf;
+      }
     }
-
-  } else if(argc==3) {
-    fname = argv[1];
-    index = atoi(argv[2]);
-    U7object f(fname,atoi(argv[2]));
-    if(f.retrieve("object.dat"))
-      cout << "Got object ok" << endl;
-    else
-      cout << "Failed to get object" << endl;
-  } else {
-    cout << "Usage:\n " << argv[0] << " -[l|x] file [index]" << endl;
+    break;
+  case EXTRACT:
+    {
+      if(argc==4) {
+	U7object f(fname,atoi(argv[3]));
+	char outfile[32];
+	sprintf(outfile,"%d.u7o",atoi(argv[3]));
+	if(!f.retrieve(outfile))
+	  cout << "Failed to get object" << endl;
+      } else {
+	U7FileManager fm;
+	U7file *f = fm.get_file_object(fname);
+	int count = f->number_of_objects(0);
+	for(index=0; index<count; index++) {
+	  U7object o(fname,index);
+	  char outfile[32];
+	  sprintf(outfile,"%d.u7o",index);
+	  o.retrieve(outfile);
+	}
+	delete f;
+      }
+    }
+    break;
+  default:
+    cout << "Usage:\n " << argv[0] << " -[l|x|c] file [index]" << endl;
+    break;
   }
   return 0;
 }
