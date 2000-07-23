c08f1d50056ed38f6d7d655a57538bdf6b591f13
Ryan Nunn <colourles@users.sourceforge.net>
2000-07-23 05:30:26 +0000

    Commented out line 327 of audio\pcb.h to stop unused variable warning

--- audio/pcb.h
+++ audio/pcb.h
@@ -324,7 +324,7 @@ public:
 		{
 		if(!l)
 			return producing?0:-1;
-		unsigned char *data=(unsigned char *)p;
+//		unsigned char *data=(unsigned char *)p;
 		lock();
 		l=Buffer.pop_front((char *)p,l);
 		unlock();
