a7fb0e67736ef76d68ef56d0bc36ad07d20de704
nadir <nadir@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-08-23 11:39:22 +0000

    2000-08-23  Tristan Tarrant  <nadir@users.sourceforge.net>
    	* tools/u7shpload.c: Can now save shapes

--- exult/trunk/tools/u7shpload.c
+++ exult/trunk/tools/u7shpload.c
@@ -22,7 +22,10 @@ static void   run        (gchar   *name,
                           gint    *nreturn_vals,
                           GParam **return_vals);
 static gint32 load_image (gchar   *filename);
-
+static gint32 save_image (gchar  *filename,
+	    gint32  image_ID,
+	    gint32  drawable_ID,
+	    gint32  orig_image_ID);
 static GRunModeType run_mode;
 static guchar   gimp_cmap[768];
 
@@ -41,7 +44,7 @@ struct u7frame {
 	gint16 rightY;
 	gint16 width;
 	gint16 height;
-
+        gint32 datalen;
 	guchar *pixels;
 };
 
@@ -56,36 +59,59 @@ MAIN ()
 static void
 query (void)
 {
-  static GParamDef load_args[] =
-  {
-    { PARAM_INT32, "run_mode", "Interactive, non-interactive" },
-    { PARAM_STRING, "filename", "The name of the file to load" },
-    { PARAM_STRING, "raw_filename", "The name entered" }
-  };
-  static GParamDef load_return_vals[] =
-  {
-    { PARAM_IMAGE, "image", "Output image" }
-  };
-  static gint nload_args = sizeof (load_args) / sizeof (load_args[0]);
-  static gint nload_return_vals = (sizeof (load_return_vals) /
-				   sizeof (load_return_vals[0]));
+	static GParamDef load_args[] = {
+		{ PARAM_INT32, "run_mode", "Interactive, non-interactive" },
+		{ PARAM_STRING, "filename", "The name of the file to load" },
+		{ PARAM_STRING, "raw_filename", "The name entered" }
+	};
+	static GParamDef load_return_vals[] = {
+		{ PARAM_IMAGE, "image", "Output image" }
+	};
+	static gint nload_args = sizeof (load_args) / sizeof (load_args[0]);
+	static gint nload_return_vals = (sizeof (load_return_vals) /
+		sizeof (load_return_vals[0]));
 
-  gimp_install_procedure ("file_shp_load",
-                          "loads files in Ultima 7 SHP format",
-                          "FIXME: write help for shp_load",
-                          "Tristan Tarrant",
-                          "Tristan Tarrant",
-                          "2000",
-                          "<Load>/SHP",
-			  NULL,
-                          PROC_PLUG_IN,
-                          nload_args, nload_return_vals,
-                          load_args, load_return_vals);
+	static GParamDef save_args[] = {
+		{ PARAM_INT32, "run_mode", "Interactive, non-interactive" },
+		{ PARAM_IMAGE,    "image",           "Image to save" },
+		{ PARAM_DRAWABLE, "drawable",        "Drawable to save" },
+		{ PARAM_STRING, "filename", "The name of the file to save" },
+		{ PARAM_STRING, "raw_filename", "The name entered" }
+	};
+	static gint nsave_args = sizeof (save_args) / sizeof (save_args[0]);
 
-  gimp_register_magic_load_handler ("file_shp_load",
-				    "shp",
-				    "",
-				    "");
+	gimp_install_procedure ("file_shp_load",
+		"loads files in Ultima 7 SHP format",
+		"FIXME: write help for shp_load",
+		"Tristan Tarrant",
+		"Tristan Tarrant",
+		"2000",
+		"<Load>/SHP",
+		NULL,
+		PROC_PLUG_IN,
+		nload_args, nload_return_vals,
+		load_args, load_return_vals);
+
+	gimp_register_magic_load_handler ("file_shp_load",
+		"shp",
+		"",
+		"");
+
+	gimp_install_procedure ("file_shp_save",
+		"Save files in Ultima 7 SHP format",
+		"FIXME: write help for shp_save",
+		"Tristan Tarrant",
+		"Tristan Tarrant",
+		"2000",
+		"<Save>/SHP",
+		"INDEXEDA",
+		PROC_PLUG_IN,
+		nsave_args, 0,
+		save_args, NULL);
+
+	gimp_register_save_handler ("file_shp_save",
+		"shp",
+		"");
 }
 
 static void
@@ -95,64 +121,108 @@ run (gchar   *name,
      gint    *nreturn_vals,
      GParam **return_vals)
 {
-  static GParam values[2];
-  GStatusType   status = STATUS_SUCCESS;
-  gint32        image_ID;
+	static GParam values[2];
+	GStatusType   status = STATUS_SUCCESS;
+	gint32        image_ID;
+	gint32        drawable_ID;
+	gint32        orig_image_ID;
 
-  run_mode = param[0].data.d_int32;
+	run_mode = param[0].data.d_int32;
 
-  *nreturn_vals = 1;
-  *return_vals  = values;
-  values[0].type          = PARAM_STATUS;
-  values[0].data.d_status = STATUS_EXECUTION_ERROR;
+	*nreturn_vals = 1;
+	*return_vals  = values;
+	values[0].type          = PARAM_STATUS;
+	values[0].data.d_status = STATUS_EXECUTION_ERROR;
 
-  if (strcmp (name, "file_shp_load") == 0)
-    {
-      image_ID = load_image (param[1].data.d_string);
+	if (strcmp (name, "file_shp_load") == 0) {
+		image_ID = load_image (param[1].data.d_string);
 
-      if (image_ID != -1)
-        {
-	  *nreturn_vals = 2;
-          values[1].type         = PARAM_IMAGE;
-          values[1].data.d_image = image_ID;
-        }
-      else
-        {
-          status = STATUS_EXECUTION_ERROR;
-        }
-    }
-  else
-    {
-      status = STATUS_CALLING_ERROR;
-    }
-
-  values[0].data.d_status = status;
+		if (image_ID != -1) {
+			*nreturn_vals = 2;
+			values[1].type         = PARAM_IMAGE;
+			values[1].data.d_image = image_ID;
+		} else {
+			status = STATUS_EXECUTION_ERROR;
+		}
+	} else if (strcmp (name, "file_shp_save") == 0) {
+		image_ID    = orig_image_ID = param[1].data.d_int32;
+		drawable_ID = param[2].data.d_int32;
+		save_image (param[3].data.d_string,
+			image_ID,
+			drawable_ID,
+			orig_image_ID);
+	} else{
+		status = STATUS_CALLING_ERROR;
+	}
+	values[0].data.d_status = status;
 }
 
 unsigned int read1(FILE *f)
 {
-  unsigned char b0;
-  b0 = fgetc(f);
-  return b0;
+	unsigned char b0;
+	b0 = fgetc(f);
+	return b0;
 }
 
 unsigned int read2(FILE *f)
 {
-  unsigned char b0, b1;
-  b0 = fgetc(f);
-  b1 = fgetc(f);
-  return (b0 + (b1<<8));
+	unsigned char b0, b1;
+	b0 = fgetc(f);
+	b1 = fgetc(f);
+	return (b0 + (b1<<8));
 }
 
 unsigned int read4(FILE *f)
 {
-  unsigned char b0, b1, b2, b3;
-  b0 = fgetc(f);
-  b1 = fgetc(f);
-  b2 = fgetc(f);
-  b3 = fgetc(f);
-  return (b0 + (b1<<8) + (b2<<16) + (b3<<24));
+	unsigned char b0, b1, b2, b3;
+	b0 = fgetc(f);
+	b1 = fgetc(f);
+	b2 = fgetc(f);
+	b3 = fgetc(f);
+	return (b0 + (b1<<8) + (b2<<16) + (b3<<24));
 }
+
+void write1(FILE *f, unsigned char b)
+{
+	fputc(b, f);
+}
+
+void write2(FILE *f, unsigned int b)
+{
+	fputc(b & 0xFF, f);
+	fputc((b >> 8) & 0xFF, f);
+}
+
+void write4(FILE *f, unsigned int b)
+{
+	fputc(b & 0xFF, f);
+	fputc((b >> 8) & 0xFF, f);
+	fputc((b >> 16) & 0xFF, f);
+	fputc((b >> 24) & 0xFF, f);
+}
+
+unsigned char *out1(unsigned char *p, unsigned char b)
+{
+    *p++ = b;
+    return p;
+}
+
+unsigned char *out2(unsigned char *p, unsigned int b)
+{
+    *p++ = b & 0xFF;
+    *p++ = (b >> 8) & 0xFF;
+    return p;
+}
+
+unsigned char *out4(unsigned char *p, unsigned int b)
+{
+    *p++ = b & 0xFF;
+    *p++ = (b >> 8) & 0xFF;
+    *p++ = (b >> 16) & 0xFF;
+    *p++ = (b >> 24) & 0xFF;
+    return p;
+}
+
 
 static gint32 load_image (gchar *filename)
 {
@@ -219,7 +289,6 @@ static gint32 load_image (gchar *filename)
 		frame->height = frame->leftY+frame->rightY+1;
 		if(frame->height>max_height)
 			max_height = frame->height;
-		printf(" Frame #%d - (%d,%d)-(%d,%d)\n",i, frame->leftX, frame->leftY, frame->rightX, frame->rightY );
 		frame->pixels = (char *)malloc(frame->width*frame->height*2);
 		memset(frame->pixels, 0, frame->width*frame->height*2);
 		while((slice=read2(fp))!=0) {
@@ -292,6 +361,198 @@ static gint32 load_image (gchar *filename)
 		free(shape.frames[i].pixels);
 	}
 
+	free(shape.frames);
+
+	return 0;
+}
+
+static int find_runs(short *runs, unsigned char *pixptr, int x,	int w)
+{
+    int runcnt = 0;
+    while (x < w && pixptr[1] != 0) {	// Stop at first transparent pixel.
+	int run = 0;		// Look for repeat.
+	while (x < w - 1 && pixptr[0] == pixptr[2]) {
+	    x++;
+	    pixptr+=2;
+	    run++;
+	}
+	if (run) {		// Repeated?  Count 1st, shift, flag.
+	    run = ((run + 1)<<1)|1;
+	    x++;		// Also pass the last one.
+	    pixptr+=2;
+	} else do {			// Pass non-repeated run of
+	    x++;
+	    pixptr+=2;
+	    run += 2;	// So we don't have to shift.
+	} while (x < w && pixptr[1] != 0 &&
+		 (x == w - 1 || pixptr[0] != pixptr[2]));
+	// Store run length.
+	runs[runcnt++] = run;
+    }
+    runs[runcnt] = 0;		// 0-delimit list.
+    return (x);
+}
+
+static int skip_transparent(unsigned char **pixptr, int x, int w)
+{
+    unsigned char *pixel = *pixptr;
+    while (x < w && pixel[1] == 0) {
+	x++;
+	pixel+=2;
+    }
+    *pixptr = pixel;
+    return x;
+}
+
+static gint32 save_image (gchar  *filename,
+	    gint32  image_ID,
+	    gint32  drawable_ID,
+	    gint32  orig_image_ID)
+{
+ 	FILE *fp;
+	gint32 shape_size;
+	gint32 hdr_size;
+	gint32 max_width;
+	gint32 max_height;
+	gint offsetX;
+	gint offsetY;
+	gchar *name_buf;
+	GDrawable *drawable;
+	GPixelRgn pixel_rgn;
+	guchar *pixptr;
+	guchar *pix;
+	guchar *out;
+	guchar *outptr;
+	int i;
+	int j;
+	int k;
+	int newx;
+	int x;
+	int y;
+	gint32 *layers;   
+	int nlayers;
+	int pos;
+
+	struct u7shape shape;
+	struct u7frame *frame;
+  
+	if (run_mode != RUN_NONINTERACTIVE) {
+		name_buf = g_strdup_printf ("Saving %s:", filename);
+		gimp_progress_init (name_buf);
+		g_free (name_buf);
+	}
+	/* get a list of layers for this image_ID */
+	layers = gimp_image_get_layers (image_ID, &nlayers);  
+
+	max_width = gimp_image_width (image_ID);
+	max_height = gimp_image_height (image_ID);
+
+	shape.num_frames = nlayers;
+	hdr_size = (nlayers+1)*4;
+	shape.frames = (struct u7frame *)malloc(sizeof(struct u7frame)*shape.num_frames);
+
+	for (k=0;k<nlayers;k++) {
+		frame = &shape.frames[k];
+		drawable = gimp_drawable_get (layers[k]);
+		gimp_drawable_offsets (layers[k], &offsetX, &offsetY);
+		frame->width = drawable->width;
+		frame->height = drawable->height;
+		frame->leftX = offsetX;
+		frame->leftY = offsetY;
+		frame->rightX = frame->width-frame->leftX-1;
+		frame->rightY = frame->height-frame->leftY-1;
+		pix = (gchar *)malloc(frame->width*frame->height*2);
+		pixptr = pix;
+		out = (gchar *)malloc(frame->width*frame->height*8);
+		outptr = out;
+		gimp_pixel_rgn_init (&pixel_rgn, drawable, 0, 0,
+				     drawable->width, drawable->height, FALSE, FALSE);
+		gimp_pixel_rgn_get_rect (&pixel_rgn, pixptr, 0, 0,
+					 drawable->width, drawable->height);
+
+		newx = 0;
+		for (y = 0; y < frame->height; y++) {
+		    for (x = 0; (x = skip_transparent(&pixptr, x, frame->width)) < frame->width; x = newx) {
+			short runs[100];
+			newx = find_runs(runs, pixptr, x, frame->width);
+			if (!runs[1] && !(runs[0]&1)) {
+			    int len = runs[0] >> 1;
+			    outptr = out2(outptr, runs[0]);
+			    outptr = out2(outptr, x - frame->leftX);
+			    outptr = out2(outptr, y - frame->leftY);
+			    for(i=0; i<len; i++) {
+				*outptr++ = *pixptr;
+				pixptr +=2;
+			    }
+			} else {
+			    outptr = out2(outptr, ((newx - x)<<1)|1);
+			    outptr = out2(outptr, x - frame->leftX);
+			    outptr = out2(outptr, y - frame->leftY);
+			    for (i = 0; runs[i]; i++) {
+				int len = runs[i]>>1;
+				if (runs[i]&1) {
+				    while (len) {
+					int c = len > 127 ? 127 : len;
+					*outptr++ = (c<<1)|1;
+					*outptr++ = *pixptr;
+					pixptr += (c*2);
+					len -= c;
+				    }
+				} else while (len > 0) {
+				    int c;
+				    c = len > 127 ? 127 : len;
+				    *outptr++ = c<<1;
+				    for(j=0; j<c; j++) {
+					*outptr++ = *pixptr;
+					pixptr +=2;
+				    }
+				    len -= c;
+				}
+			    }
+			}
+		    }
+		}
+		outptr = out2(outptr, 0);			// End with 0 length.
+		frame->datalen = outptr - out;
+		frame->pixels = (gchar *) malloc(frame->datalen);
+		memcpy(frame->pixels, out, frame->datalen);
+		free(out);
+		free(pix);
+		gimp_drawable_detach(drawable);
+	}
+
+	fp = fopen (filename, "wb");
+	if (!fp) {
+		g_message ("SHP: can't create \"%s\"\n", filename);
+		return -1;
+	}
+
+	shape_size = 0;
+	write4(fp, shape_size);	// Fill in later
+	for(i=0; i<shape.num_frames; i++) 
+	    write4(fp, 0);	// Fill in later
+	for(i=0; i<shape.num_frames; i++) {
+	    frame = &shape.frames[shape.num_frames-i-1];
+	    pos = ftell(fp);	// Get the frame offset
+	    fseek(fp, (i+1)*4, SEEK_SET);
+	    write4(fp, pos);	// Write it in the right place
+	    fseek(fp, pos, SEEK_SET);
+	    write2(fp, frame->rightX);
+	    write2(fp, frame->leftX);
+	    write2(fp, frame->leftY);
+	    write2(fp, frame->rightY);
+	    fwrite(frame->pixels, 1, frame->datalen, fp);
+	}
+
+	pos = ftell(fp);	// Get the file size
+	fseek(fp, 0, SEEK_SET);
+	write4(fp, pos);	// Write it in the right place
+
+	fclose(fp);
+	
+	for(i=0; i<shape.num_frames; i++) {
+	    free(shape.frames[i].pixels);
+	}
 	free(shape.frames);
 
 	return 0;
