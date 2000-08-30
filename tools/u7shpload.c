d145f536da7f1c5be7c93e277f59900f6336b273
nadir <nadir@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-08-30 13:49:03 +0000

    2000-08-30  Tristan Tarrant  <nadir@users.sourceforge.net>
    	* tools/u7shpload.c: Prompt user for a palette file for loading.
    	Decent colours at last !!!

--- tools/u7shpload.c
+++ tools/u7shpload.c
@@ -22,6 +22,9 @@ static void   run        (gchar   *name,
                           GParam  *param,
                           gint    *nreturn_vals,
                           GParam **return_vals);
+static void   load_palette(gchar *filename);
+static void   choose_palette (void);
+static void   palok_callback          (GtkWidget *widget, gpointer   data);
 static gint32 load_image (gchar   *filename);
 static gint32 save_image (gchar  *filename,
 	    gint32  image_ID,
@@ -136,7 +139,8 @@ run (gchar   *name,
 	values[0].data.d_status = STATUS_EXECUTION_ERROR;
 
 	if (strcmp (name, "file_shp_load") == 0) {
-		printf("Loading %s...\n", param[1].data.d_string);
+		gimp_ui_init ("u7shp", FALSE);
+		choose_palette();
 		image_ID = load_image (param[1].data.d_string);
 
 		if (image_ID != -1) {
@@ -225,54 +229,99 @@ unsigned char *out4(unsigned char *p, unsigned int b)
     return p;
 }
 
+static void set_gray_palette()
+{
+	int i;
+
+	for(i=0;i<256;i++) {
+		gimp_cmap[i*3]=i;
+		gimp_cmap[i*3+1]=i;
+		gimp_cmap[i*3+2]=i;
+	}
+}
+
+static void load_palette (gchar *filename)
+{
+	FILE *fp;
+	long len;
+	int i;
+	
+	fp = fopen (filename, "rb");
+	if (!fp) {
+		set_gray_palette();
+		return;
+	}
+	fseek(fp, 0, SEEK_END);
+	len = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	if(len==768) {
+		for(i=0;i<256;i++) {
+			gimp_cmap[i*3]=read1(fp) << 2;
+			gimp_cmap[i*3+1]=read1(fp) << 2;
+			gimp_cmap[i*3+2]=read1(fp) << 2;
+		}
+	} else if(len==1536) {
+		for(i=0;i<256;i++) {
+			gimp_cmap[i*3]=read1(fp) << 2;
+			read1(fp);
+			gimp_cmap[i*3+1]=read1(fp) << 2;
+			read1(fp);
+			gimp_cmap[i*3+2]=read1(fp) << 2;
+			read1(fp);
+		}
+	} else {
+		set_gray_palette();	
+	}
+	fclose(fp);
+}
+
+static void file_sel_delete( GtkWidget *widget, GtkWidget **file_sel )
+{
+	gtk_widget_destroy( *file_sel );
+	*file_sel = NULL;
+}
+
+static void file_selected( GtkWidget *widget, gboolean *selected )
+{
+	*selected = TRUE;
+}
+
+gchar* file_select( gchar *title )
+{
+	GtkWidget *file_sel;
+	gchar *filename;
+	gboolean selected = FALSE;
+
+	file_sel = gtk_file_selection_new( title );
+	gtk_window_set_modal( GTK_WINDOW( file_sel ), TRUE );
+
+	gtk_signal_connect( GTK_OBJECT( file_sel ), "destroy", 
+                            GTK_SIGNAL_FUNC( file_sel_delete ), &file_sel );
+	
+        gtk_signal_connect( GTK_OBJECT( GTK_FILE_SELECTION( file_sel )->cancel_button ), "clicked", GTK_SIGNAL_FUNC( file_sel_delete ), &file_sel );
+        
+        gtk_signal_connect( GTK_OBJECT( GTK_FILE_SELECTION( file_sel )->ok_button ), "clicked", GTK_SIGNAL_FUNC( file_selected ), &selected );
+
+        gtk_widget_show( file_sel );
+
+	while( ( ! selected ) && ( file_sel ) )
+		gtk_main_iteration();
+
+	/* canceled or window was closed */
+	if( ! selected )
+		return NULL;
+
+	/* ok */
+	filename = g_strdup( gtk_file_selection_get_filename( GTK_FILE_SELECTION( file_sel ) ) );
+	gtk_widget_destroy( file_sel );
+	return filename;
+}
+
+
+
 static void choose_palette()
 {
-	GtkWidget *dlg;
-	GtkWidget *label;
-	GtkWidget *frame;
-	GtkWidget *vbox;
-	
-	dlg = gimp_dialog_new ("SHP Loader", "shp_loader",
-		NULL, NULL,
-		GTK_WIN_POS_MOUSE,
-		FALSE, FALSE, FALSE,
-
-		"OK", NULL,
-		NULL, NULL, NULL, TRUE, FALSE,
-		"Cancel", gtk_widget_destroy,
-		NULL, 1, NULL, FALSE, TRUE,
-		NULL);
-
-  gtk_signal_connect (GTK_OBJECT (dlg), "destroy",
-		      GTK_SIGNAL_FUNC (gtk_main_quit),
-		      NULL);
-
-  frame = gtk_frame_new (NULL);
-  gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_ETCHED_IN);
-  gtk_container_set_border_width (GTK_CONTAINER (frame), 6);
-  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dlg)->vbox), frame, TRUE, TRUE, 0);
-
-  vbox = gtk_vbox_new (FALSE, 4);
-  gtk_container_set_border_width (GTK_CONTAINER (vbox), 4);
-  gtk_container_add (GTK_CONTAINER (frame), vbox);
-  
-  label= gtk_label_new ("The image which you are trying to save as a GIF\n"
-			  "contains layers which extend beyond the actual\n"
-			  "borders of the image.  This isn't allowed in GIFs,\n"
-			  "I'm afraid.\n\n"
-			  "You may choose whether to crop all of the layers to\n"
-			  "the image borders, or cancel this save.");
-  gtk_box_pack_start (GTK_BOX (vbox), label, TRUE, TRUE, 0);
-  gtk_widget_show (label);
-
-  gtk_widget_show (vbox);
-  gtk_widget_show (frame);
-
-  gtk_widget_show (dlg);
-
-  gtk_main ();
-  gdk_flush ();
-
+	load_palette(file_select("Choose palette"));
 }
 
 
@@ -371,11 +420,6 @@ static gint32 load_image (gchar *filename)
 				}
 			}
 		}
-	}
-	for(i=0;i<256;i++) {
-		gimp_cmap[i*3]=i;
-		gimp_cmap[i*3+1]=i;
-		gimp_cmap[i*3+2]=i;
 	}
 	image_ID = gimp_image_new (max_width, max_height, INDEXED);
 	gimp_image_set_filename (image_ID, filename);
