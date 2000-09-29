eb6ac196acedb8f8e85314ddd4d1a42a25c1ab68
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-09-29 20:04:44 +0000

    updated lots of body shapes

--- bodies.cc
+++ bodies.cc
@@ -39,70 +39,68 @@ using std::size_t;
 
 short Body_lookup::table[] = {
 				// Monsters:
-	491,	762,	13,	 // acid slug
-	492,	892,	13,	 // alligator
-	493,	762,	9,	 // bat   ??Unsure
-	403,	762,	2,	 // Batlin
-	482,	762,	2,	 // Batlin
-	494,	762,	2,	 // bee
-	716,	762,	2,	 // bird
-	495,	892,	18,	 // cat
-	498,	762,	2,	 // chicken
-	499,	892,	21,	 // corpser
-	500,	778,	11,	 // cow
-	259,	762,	2,	 // fighter
-	154,	762,	22,	 // mage
-	247,	762,	23,	 // paladin
-	401,	762,	25,	 // pirate
-	501,	762,	2,	 // cyclops
-	502,	778,	12,	 // deer
-	496,	892,	24,	 // dog
-	504,	778,	7,	 // dragon
-	505,	778,	8,	 // drake
-	784,	762,	2,	 // emp
-	230,	762,	2,	 // ethereal monster
-	929,	762,	2,	 // Fellowship member
-	382,	892,	15,	 // Kissme
-	952,	762,	2,	 // The Ferryman
-	509,	892,	26,	 // fish
-	510,	892,	27,	 // fox
-	883,	762,	1,	 // wingless Gargoyle
-	511,	762,	2,	 // gazer
-	337,	762,	0,	 // ghost	(invisible).
-	299,	762,	0,	 // ghost
-	317,	762,	0,	 // ghost
-	1015,	414,	4,	 // Golem
-	513,	762,	4,	 // gremlin
-	946,	762,	23,	 // guard
-	532,	762,	5,	 // harpie
-	514,	762,	6,	 // headless
-	727,	778,	1,	 // horse
-	381,	762,	2,	 // three headed hydra
-	517,	892,	15,	 // insects
-	354,	762,	8,	 // liche
-	519,	762,	8,	 // liche
-	466,	414,	12,	 // Lord British
-	884,	762,	2,	 // Fellowship member
-	661,	762,	2,	 // mongbat
-	521,	762,	2,	 // mouse
-	394,	762,	23,	 // guard
-	811,	762,	10,	 // rabbit
-	523,	762,	11,	 // rat
-	524,	762,	12,	 // reaper
-	706,	762,	26,	 // scorpion
-	525,	762,	2,	 // sea serpent
-	970,	762,	14,	 // sheep
-	528,	762,	16,	 // skeleton
-	529,	762,	0,	 // slime (invisible).
-	530,	762,	18,	 // snake
-	865,	762,	19,	 // spider
-	753,	762,	20,	 // stone harpie
-	536,	762,	0,	 // tentacles (invisible).
-	806,	762,	23,	 // guard
-	533,	778,	4,	 // troll
-	534,	762,	2,	 // wisp
-	537,	762,	21,	 // wolf
-	447,	762,	2,	 // wounded man
+	491,	892,	12,	// acid slug
+	492,	892,	13,	// alligator
+	493,	762,	9,	// bat   (no body!)
+	494,	892,	15,	// bee
+	716,	892,	16,	// bird
+	495,	892,	18,	// cat
+	498,	892,	20,	// chicken
+	499,	892,	21,	// corpser
+	500,	778,	11,	// cow
+	501,	778,	6,	// cyclops
+	502,	778,	12,	// deer
+	496,	892,	24,	// dog
+	504,	778,	7,	// dragon
+	505,	778,	8,	// drake
+	784,	892,	25,	// emp
+	230,	762,	2,	// ethereal monster (no body!)
+	884,	892,	5,	// Fellowship member
+	929,	400,	10,	// Fellowship member
+	155,	762,	2,	// Ferryman
+	259,	400,	20,	// fighter
+	509,	892,	26,	// fish
+	510,	892,	27,	// fox
+	883,	762,	1,	// wingless Gargoyle
+	511,	762,	2,	// gazer
+	337,	762,	0,	// ghost	(invisible).
+	299,	762,	0,	// ghost
+	317,	762,	0,	// ghost
+	1015,	414,	4,	// Golem
+	513,	762,	4,	// gremlin
+	394,	892,	10,	// guard
+	806,	892,	7,	// guard
+	946,	892,	8,	// guard
+	532,	762,	5,	// harpie
+	514,	762,	6,	// headless
+	727,	778,	1,	// horse
+	381,	778,	3,	// three headed hydra
+	382,	414,	7,	// Kissme
+	517,	892,	15,	// insects
+	354,	762,	8,	// liche
+	519,	762,	8,	// liche (Horatius?)
+	466,	414,	12,	// Lord British
+	154,	762,	22,	// mage
+	661,	762,	9,	// mongbat
+	521,	762,	10,	// mouse
+	247,	762,	23,	// paladin
+	401,	762,	25,	// pirate
+	811,	892,	17,	// rabbit
+	523,	762,	11,	// rat
+	524,	762,	12,	// reaper
+	706,	762,	26,	// scorpion
+	525,	762,	2,	// sea serpent (no body!)
+	970,	762,	14,	// sheep
+	528,	762,	16,	// skeleton
+	529,	762,	0,	// slime (invisible).
+	530,	762,	18,	// snake
+	865,	762,	19,	// spider
+	753,	762,	20,	// stone harpie
+	536,	762,	0,	// tentacles (invisible).
+	533,	778,	5,	// troll
+	534,	762,	0,	// wisp
+	537,	762,	21,	// wolf
+	447,	892,	11,	// wounded man
 				// NCPs:
 	318,	400,	5,	// Sage.
 	319,	400,	9,	// Male peasant.
@@ -111,8 +109,12 @@ short Body_lookup::table[] = {
 	454,	400,	12,	// Female shopkeeper.
 	455,	400,	11,	// Male shopkeeper.
 	456,	400,	14,	// Female noble.
+	457,	400,	15,	// Male gypsy.
 	458,	400,	16,	// Pirate.
+	459,	400,	17,	// Female gypsy.
 	401,	400,	16,	// Pirate.
+	460,	400,	18,	// Male ranger
+	461,	400,	19,	// Female ranger
 	462,	400,	20,	// Male fighter.
 	463,	400,	21,	// Female fighter.
 	465,	400,	3,	// Iolo.
@@ -122,8 +124,10 @@ short Body_lookup::table[] = {
 	720,	400,	23,	// Guard.
 	882,	892,	1,	// Abraham.
 	881,	892,	2,	// Elizabeth.
-	403,	892,	3,	// Batlin.
-	482,	892,	3,	// Batlin.
+#	403,	892,	3,	// Batlin.
+	403,	414,	16,	// Batlin.
+#	482,	892,	3,	// Batlin.
+	403,	414,	16,	// Batlin.
 	805,	892,	4,	// Forksis.
 	506,	892,	19	// Hook.
 	};
