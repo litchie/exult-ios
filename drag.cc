6a63f963f7359d52003b335ab797f92216bb7c97
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-09-30 17:22:53 +0000

    added some extra functionality to hack_mover

--- drag.cc
+++ drag.cc
@@ -328,10 +328,10 @@ int Game_window::drop_at_lift
 					// Allow drop up to 5.
 	if (!Chunk_object_list::is_blocked(info.get_3d_height(), at_lift,
 		tx - xtiles + 1, ty - ytiles + 1, xtiles, ytiles, 
-					lift, MOVE_FLY, 5) &&
+					lift, MOVE_FLY, 5) && (hack_mover ||
 					// Check for path to location.
 	    Fast_pathfinder_client::is_grabable(
-		main_actor->get_abs_tile_coord(), Tile_coord(tx, ty, lift)))
+		main_actor->get_abs_tile_coord(), Tile_coord(tx, ty, lift))))
 		{
 		to_drop->set_lift(lift);
 		int rtx = tx%tiles_per_chunk, rty = ty%tiles_per_chunk;
