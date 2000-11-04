409e2dad0fa415453bcb162198768f21ce17ba76
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2000-11-04 17:25:43 +0000

    fixed case bug in includes

--- audio/Midi.cc
+++ audio/Midi.cc
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "gamewin.h"
 #include "game.h"
 #include "conv.h"
-#include "audio.h"
+#include "Audio.h"
 
 #include "Configuration.h"
 extern	Configuration	*config;
