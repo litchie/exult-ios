bd7147a2915c0507817d7bb657d94d7cc3e40dc7
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-11-07 18:38:34 +0000

    fixed 'cheat' init. problem

--- cheat.h
+++ cheat.h
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef CHEAT_H
 #define CHEAT_H
 
-#include "gamewin.h"
+class Game_window;
 
 class Cheat {
  public:
@@ -27,6 +27,7 @@ class Cheat {
   ~Cheat();
 
   void init (void);
+  void init_gwin (void);
 
 private:
   Game_window *gwin;
