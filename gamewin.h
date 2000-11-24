3a9c5d3c1643701361577aba9a88b368eab746bb
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2000-11-24 23:54:25 +0000

    fixed gump buttons passing clicks to underlying items.

--- gamewin.h
+++ gamewin.h
@@ -591,7 +591,7 @@ public:
 					// Start dragging.
 	int start_dragging(int x, int y);
 	int drag(int x, int y);		// During dragging.
-	void drop_dragged(int x, int y, int moved);// Done dragging.
+	bool drop_dragged(int x, int y, int moved);// Done dragging.
 					// Paint text using "fonts.vga".
 	int paint_text_box(int fontnum, const char *text, int x, int y, int w, 
 		int h, int vert_lead = 0, int pbreak = 0);
