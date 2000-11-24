116284b946217448351eb48a04a4ae788abcde7e
fingolfin <fingolfin@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-11-24 22:40:39 +0000

    * forgot to add #include "exceptions.h", fixed

--- exult/trunk/audio/midi_drivers/win_midiout.h
+++ exult/trunk/audio/midi_drivers/win_midiout.h
@@ -34,6 +34,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "xmidi.h"
 #include "SDL_syswm.h"
 #include "mmsystem.h"
+#include "exceptions.h"
 
 class	Windows_MidiOut : virtual public MidiAbstract
 {
