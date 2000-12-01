5aa7b9d376ab59f3cf3a15cd00df4a1ad0de7faa
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2000-12-01 04:41:31 +0000

    frameseq.h

--- usecode/ucsched.cc
+++ usecode/ucsched.cc
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Audio.h"
 #include "barge.h"
 #include "game.h"
+#include "frameseq.h"
 
 #include <iostream>
 #include <iomanip>
