7d30c0fb8d78a3ced3586198ffdd9407fb710d26
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-01-03 05:27:42 +0000

    Coding

---
+++ exult/trunk/usecode/compiler/ucfun.cc
@@ -0,0 +1,93 @@
+/**
+ **	Ucfun.cc - Usecode compiler function.
+ **
+ **	Written: 1/2/01 - JSF
+ **/
+
+/*
+Copyright (C) 2000 The Exult Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <stdio.h>
+#include "ucfun.h"
+#include "ucloc.h"
+
+/*
+ *	Create function.
+ */
+
+Uc_function::Uc_function
+	(
+	Uc_function_symbol *p
+	) : top(0), proto(p), cur_scope(&top), num_locals(0)
+	{
+	const vector<char *>& parms = proto->get_parms();
+	for (vector<char *>::const_iterator it = parms.begin();
+				it != parms.end(); it++)
+		add_symbol(*it);
+	}
+
+/*
+ *	Add a new variable to the current scope.
+ */
+
+Uc_symbol *Uc_function::add_symbol
+	(
+	char *nm
+	)
+	{
+	Uc_symbol *sym = search(nm);
+	if (sym)			// Already in scope?
+		{
+		char msg[180];
+		sprintf(msg, "Symbol '%s' already declared", nm);
+		Uc_location::yyerror(msg);
+		return sym;
+		}
+					// Create & assign slot.
+	sym = new Uc_symbol(nm, num_locals++);
+	cur_scope->add(sym);
+	return sym;
+	}
+
+/*
+ *	Add a new string constant to the current scope.
+ */
+
+Uc_symbol *Uc_function::add_string_symbol
+	(
+	char *nm,
+	char *text
+	)
+	{
+	Uc_symbol *sym = search(nm);
+	if (sym)			// Already in scope?
+		{
+		char msg[180];
+		sprintf(msg, "Symbol '%s' already declared", nm);
+		Uc_location::yyerror(msg);
+		return sym;
+		}
+					// Create & assign slot.
+	sym = new Uc_string_symbol(nm, text_data.length());
+	text_data.append(text);
+	cur_scope->add(sym);
+	return sym;
+	}
+
+
+
