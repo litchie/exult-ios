556325b247eaef2d49953deb139d0d2c768f388d
takhisis <takhisis@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-01-05 21:48:14 +0000

    More of the usecode decompiler

---
+++ exult/trunk/usecode/ucxt/include/opcodec_.h
@@ -0,0 +1,216 @@
+
+#ifndef OPCODEC__H
+#define OPCODEC__H
+
+#include <string>
+#include <vector>
+#include <iomanip>
+
+inline unsigned int calc_rel_offset(const unsigned int offset,
+                                    const unsigned int params_size,
+                                    const int reloffset)
+{
+      // relative offset calculation =
+      //   1           (size of id)
+      // + offset      (offset from start of procedure)
+      // + params_size (number of bytes until end of function)
+      // + reloffset
+      return 1 + offset + params_size + reloffset;
+}
+;
+class Opcode
+{
+  public:
+    Opcode(const unsigned int offset, const unsigned int id, const vector<unsigned int> &params)
+          : _offset(offset), _id(id), _params(params) {};
+    virtual ~Opcode() {};
+    virtual void print_asm(ostream &o)=0;
+    virtual void print(ostream &o) { print_asm(o); };
+    virtual void pass1(const vector<unsigned int> &_externs) {};
+    friend ostream &operator<<(ostream &o, const Opcode &op);
+
+    unsigned int offset() const { return _offset; };
+
+    enum
+    {
+      UNKNOWN_00   = 0x00,
+      UNKNOWN_01   = 0x01,
+
+      NEXT         = 0x02,
+
+      UNKNOWN_03   = 0x03,
+
+      ASK          = 0x04,
+      JNE          = 0x05,
+      JMP          = 0x06,
+      JMPA         = 0x07,
+
+      UNKNOWN_08   = 0x08,
+
+      ADD          = 0x09,
+      SUB          = 0x0A,
+      DIV          = 0x0B,
+      MUL          = 0x0C,
+      MOD          = 0x0D,
+      AND          = 0x0E,
+      OR           = 0x0F,
+      NOT          = 0x10,
+
+      UNKNOWN_11   = 0x11,
+
+      POP          = 0x12,
+      PUSH_TRUE    = 0x13,
+      PUSH_FALSE   = 0x14,
+
+      UNKNOWN_15   = 0x15,
+
+      CMPGT        = 0x16,
+      CMPLT        = 0x17,
+      CMPGE        = 0x18,
+      CMPLE        = 0x19,
+      CMPNE        = 0x1A,
+
+      UNKNOWN_1B   = 0x1B,
+
+      ADDSI        = 0x1C,
+      PUSHS        = 0x1D,
+      ARRC         = 0x1E,
+      PUSHI        = 0x1F,
+
+      UNKNOWN_20   = 0x20,
+
+      PUSH         = 0x21,
+      CMPEQ        = 0x22,
+
+      UNKNOWN_23   = 0x23,
+
+      CALL_EXTERN  = 0x24,
+      RET          = 0x25,
+      AGET         = 0x26,
+
+      UNKNOWN_27   = 0x27,
+      UNKNOWN_28   = 0x28,
+      UNKNOWN_29   = 0x29,
+      UNKNOWN_2A   = 0x2A,
+      UNKNOWN_2B   = 0x2B,
+
+      EXIT2        = 0x2C,
+      POPR         = 0x2D,
+      ENUM         = 0x2E,
+      ADDSV        = 0x2F,
+      IN           = 0x30,
+
+      UNKNOWN_31   = 0x31,
+      RETR         = 0x32,
+      SAY          = 0x33,
+
+      UNKNOWN_34   = 0x34,
+      UNKNOWN_35   = 0x35,
+      UNKNOWN_36   = 0x36,
+      UNKNOWN_37   = 0x37,
+
+      CALLIS       = 0x38,
+      CALLI        = 0x39,
+
+      UNKNOWN_3A   = 0x3A,
+      UNKNOWN_3B   = 0x3B,
+      UNKNOWN_3C   = 0x3C,
+      UNKNOWN_3D   = 0x3D,
+
+      PUSH_ITEMREF = 0x3E,
+      EXIT         = 0x3F,
+      CLA          = 0x40,
+
+      UNKNOWN_41   = 0x41,
+
+      PUSHF        = 0x42,
+      POPF         = 0x43,
+      PUSHBI       = 0x44,
+
+      UNKNOWN_45   = 0x45,
+
+      APUT         = 0x46,
+      CALLE        = 0x47,
+      PUSH_EVENTID = 0x48,
+
+      UNKNOWN_49   = 0x49,
+
+      ARRA         = 0x4A,
+      POP_EVENTID  = 0x4B,
+
+      OPEND
+    };
+
+    // Iama's (I am a...) always false unless overridden
+    virtual bool IsaRelativeJump() const { return false; };
+    virtual bool IsaFlag() const { return false; };
+    virtual bool IsUnknown() const { return false; };
+    // I's are always false unless overridden
+    virtual bool I_Pop() const { return false; };
+    virtual bool I_Push() const { return false; };
+
+    // amusing... ;)
+//    static const char * const SPACER = " ";
+    static const char * const SPACER;
+
+  protected:
+    void indent(ostream &o)
+    {
+      for(unsigned int i=0; i<indent_level; i++)
+        o << "  ";
+    };
+
+    unsigned int _offset;
+    unsigned int _id;
+
+  private:
+    static unsigned int indent_level;
+    vector<unsigned int> _params;
+
+};
+
+//const char * const Opcode::SPACER = "\t";
+
+inline ostream &operator<<(ostream &o, const Opcode &op)
+{
+  o << setw(2) << op._id;
+
+  for(unsigned int i=0; i<op._params.size(); i++)
+    o << ' ' << setw(2) << op._params[i];
+
+  return o;
+}
+
+class OpcodeGenericJump : public Opcode
+{
+  public:
+    OpcodeGenericJump(const unsigned int offset, const unsigned int id, const vector<unsigned int> &params)
+              : Opcode(offset, id, params)
+    {
+    };
+    virtual ~OpcodeGenericJump() {};
+
+    unsigned int target_offset() const { return _target_offset; };
+
+  protected:
+    unsigned int _target_offset;
+};
+
+class OpcodeGenericFlag : public Opcode
+{
+  public:
+    OpcodeGenericFlag(const unsigned int offset, const unsigned int id, const vector<unsigned int> &params)
+              : Opcode(offset, id, params)
+    {
+    };
+    virtual ~OpcodeGenericFlag() {};
+    virtual bool IsaFlag() const { return true; };
+
+    unsigned int flag() const { return _flag; };
+
+  protected:
+    unsigned int _flag;
+};
+
+#endif
+
---
+++ exult/trunk/usecode/ucxt/include/ucfunc.h
@@ -0,0 +1,201 @@
+
+#ifndef UCFUNC_H
+#define UCFUNC_H
+
+#include <map>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include "ucc.h"
+#include "opcodec.h"
+
+class FlagData
+{
+  public:
+    //enum { SETFLAG=false, GETFLAG=true };
+    /* SETFLAG=UCC_POPF, GETFLAG=UCC_PUSHF */
+    enum { POP=false, PUSH=true};
+
+    FlagData(const long func, const unsigned short offset,
+             const unsigned short flag, const bool access)
+            : _func(func), _offset(offset), _flag(flag), _access(access) {};
+
+    long func() const { return _func; };
+    unsigned short offset() const { return _offset; };
+    unsigned short flag() const { return _flag; };
+    bool access() const { return _access; };
+
+  private:
+    long           _func;
+    unsigned short _offset;
+    unsigned short _flag;
+    bool           _access;
+};
+
+class SortFlagDataLessFlag
+{
+  public:
+    bool operator()(const FlagData &fd1, const FlagData &fd2) const
+    {
+       return (fd1.flag()<fd2.flag()) ? true :
+                  (fd1.flag()>fd2.flag()) ? false :
+                      (fd1.func()<fd2.func()) ? true :
+                          (fd1.func()>fd2.func()) ? false :
+                              (fd1.offset()<fd2.offset());
+    };
+    bool operator()(const FlagData *fd1, const FlagData *fd2) const
+    {
+       return (fd1->flag()<fd2->flag()) ? true :
+                  (fd1->flag()>fd2->flag()) ? false :
+                      (fd1->func()<fd2->func()) ? true :
+                          (fd1->func()>fd2->func()) ? false :
+                              (fd1->offset()<fd2->offset());
+    };
+};
+
+class SortFlagDataLessFunc
+{
+  public:
+    bool operator()(const FlagData &fd1, const FlagData &fd2) const
+    {
+       return (fd1.func()<fd2.func()) ? true :
+                  (fd1.func()>fd2.func()) ? false :
+                      (fd1.flag()<fd2.flag()) ? true :
+                          (fd1.flag()>fd2.flag()) ? false :
+                              (fd1.offset()<fd2.offset());
+    };
+    bool operator()(const FlagData *fd1, const FlagData *fd2) const
+    {
+       return (fd1->func()<fd2->func()) ? true :
+                  (fd1->func()>fd2->func()) ? false :
+                      (fd1->flag()<fd2->flag()) ? true :
+                          (fd1->flag()>fd2->flag()) ? false :
+                              (fd1->offset()<fd2->offset());
+    };
+};
+
+class UCFunc
+{
+  public:
+    UCFunc() : _opcode_count(256), _unknown_opcode_count(256, 0), _unknown_intrinsic_count(256, 0) {};
+    ~UCFunc();
+    void process_old(FILE* f, long func, int* found,
+//                          unsigned char* opcode_buf,
+                          unsigned char* intrinsic_buf,
+                          bool scan_mode,
+                          unsigned long opcode,
+                          unsigned long intrinsic,
+                          unsigned int &uc_funcid,
+//                          vector<UCc> &uc_codes,
+                          const char** func_table);
+    void process_data_seg();
+    void process_code_seg(//unsigned char* opcode_buf,
+                          unsigned char* intrinsic_buf,
+                          int mute,
+                          int count_all_opcodes,
+                          int count_all_intrinsic,
+//                          vector<UCc> &uc_codes,
+                          const char** func_table);
+    unsigned short print_opcode(unsigned char* ptrc, unsigned short coffset,
+                            unsigned char* pdataseg,
+                            unsigned short* pextern,
+                            unsigned short externsize,
+//                            unsigned char* opcode_buf,
+                            unsigned char* intrinsic_buf,
+                            int mute,
+                            int count_all_opcodes,
+                            int count_all_intrinsic,
+//                            vector<UCc> &uc_codes,
+                            const char** func_table);
+    unsigned short print_opcode_old(unsigned char* ptrc, unsigned short coffset,
+                            unsigned char* pdataseg,
+                            unsigned short* pextern,
+                            unsigned short externsize,
+//                            unsigned char* opcode_buf,
+                            unsigned char* intrinsic_buf,
+                            int mute,
+                            int count_all_opcodes,
+                            int count_all_intrinsic,
+//                            vector<UCc> &uc_codes,
+                            const char** func_table);
+
+    //void genflags(const vector<UCc> &uc_codes);
+    const vector<FlagData *> &flags() const { return _flagcount; };
+
+    // LEGACY
+    const vector<UCc> &codes() const { return _codes; };
+
+    // temporary until it's removed from ucdump.cc
+    const vector<unsigned int> &externs() const { return _externs; };
+    const map<unsigned int, string, less<unsigned int> > &data() const
+             { return _data; };
+    unsigned int localc() const { return _localc; };
+    unsigned int argc() const { return _argc; };
+    unsigned short funcid() const { return _funcid; };
+    long offset() const { return _offset; };
+    unsigned short funcsize() const { return _funcsize; };
+    unsigned short datasize() const { return _datasize; };
+    unsigned short codesize() const { return _funcsize - _datasize; };
+
+  private:
+    unsigned short read_ushort();
+    unsigned short read_ushort(const unsigned char *buff);
+    void read_vchars(char *buffer, const unsigned long nobytes);
+    void read_vbytes(unsigned char *buffer, const unsigned long nobytes);
+
+    // decompiling functions
+    void do_decompile();
+    void do_print();
+    void genflags();
+
+    void re_order();
+
+    void print_asm();
+    void print_asm_data();
+
+    void print_c_externs();
+    void print_c_head();
+    void print_c_local();
+    void print_c_body();
+    void print_c_tail();
+
+    string extern_tostr(const unsigned int uc_extern);
+    // /decompiling functions
+
+    // temp file manipulation functions
+    long ftell() const { return ::ftell(_file); };
+    int fseek(const long offset, const int mode) { return ::fseek(_file, offset, mode); };
+    bool eof() const { return feof(_file); };
+    int get() { return getc(_file); };
+
+    // /temp file manipulation functions
+
+    unsigned short _funcid;
+    unsigned short _funcsize;
+    unsigned short _datasize;
+
+    unsigned int _argc; // number of function parameters
+    unsigned int _localc; // number of local variables
+
+    long           _offset;
+    long           _code_offset; // offset to start of code segment in file
+
+    FILE          *_file;
+
+    vector<FlagData *>   _flagcount;
+    vector<Opcode *>     _raw_opcodes;
+    vector<Label *>      _opcodes;
+    vector<unsigned int> _externs;
+    map<unsigned int, string, less<unsigned int> > _data;
+    vector<unsigned int> _opcode_count;
+
+    // currently not used
+    vector<unsigned int> _unknown_opcode_count;
+    vector<unsigned int> _unknown_intrinsic_count;
+
+    // LEGACY: remove when old c output format is unused
+    vector<UCc> _codes;
+};
+
+#endif
+
