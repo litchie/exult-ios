fe05033217c444f8c0d2d0c9d942f2002b2cc90c
Max Horn <max@quendi.de>
2001-01-11 21:19:47 +0000

    * contains.cc: added #include "files/utils.h"

--- objs/contain.cc
+++ objs/contain.cc
@@ -30,6 +30,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "objiter.h"
 #include "game.h"
 #include "ucmachine.h"
+#include "files/utils.h"
 
 using std::rand;
 using std::ostream;
