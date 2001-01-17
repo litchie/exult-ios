102e3bf4193af8f52ca7a3bc16f8d35814d46dde
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-01-17 23:13:09 +0000

    fixing win32 compile error

--- exult/trunk/gumps/gump_utils.h
+++ exult/trunk/gumps/gump_utils.h
@@ -25,6 +25,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "mouse.h"
 
+#ifndef XWIN
+#include "SDL_timer.h"
+#endif
+
 class Modal_gump;
 class Game_window;
 
