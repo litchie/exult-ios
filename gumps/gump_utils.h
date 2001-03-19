37f28df2cadf7d75f2aeff84f1c14aac4b1c187c
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-03-19 21:05:33 +0000

    * gumps/gump_utils.h: added a few includes for OpenBSD

--- gumps/gump_utils.h
+++ gumps/gump_utils.h
@@ -23,6 +23,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/time.h>
 #endif
 
+#ifdef OPENBSD
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
 #include "mouse.h"
 
 #ifndef XWIN
