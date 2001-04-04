024b298ab9427b7dc2231f66fe34eb5a88bd8ee6
Jeff Freedman <jsf@users.sourceforge.net>
2001-04-04 07:31:01 +0000

    Ship bugs

--- objs/flags.h
+++ objs/flags.h
@@ -36,6 +36,7 @@ namespace  Obj_flags
 		tremor = 12,		// ??Earthquake??
 		dancing = 15,		// ??Not sure.
 		dont_render = 16,	// Completely invisible.
+		si_on_moving_barge = 17,// SI's version of 10?
 		okay_to_move = 18,	// ??Guess: for Usecode-created items.
 		okay_to_land = 21,	// Used for flying-carpet.
 		confused = 25,		// ??Guessing.
