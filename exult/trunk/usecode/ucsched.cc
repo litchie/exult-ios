d7620818b155fe2266412b35570a9833f9c922a6
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-04-17 03:01:18 +0000

    More SI fixes (fade palette when sleeping)

--- exult/trunk/usecode/ucsched.cc
+++ exult/trunk/usecode/ucsched.cc
@@ -206,7 +206,9 @@ void Scheduled_usecode::handle_event
 					// ?? Guessing at time.
 //			delay = 200*(delayval.get_int_value()); 9/17/00:
 // NOTE: Changing this can have a major impact!
-			delay = 400*(delayval.get_int_value());
+//			delay = 400*(delayval.get_int_value());
+// Changed Apr. 16, 2001 to:
+			delay = 250*(delayval.get_int_value());
 			break;		
 			}
 #if 0
