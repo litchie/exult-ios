5c0f3dc396613b94a68f7e11c8fba714b9d041e5
Jeff Freedman <jsf@users.sourceforge.net>
2001-06-16 18:19:51 +0000

    Fixed SI intrinsic

--- objs/flags.h
+++ objs/flags.h
@@ -27,6 +27,7 @@ namespace  Obj_flags
 		asleep = 1,
 		charmed = 2,
 		cursed = 3,
+					// 4=paralyzed (SI), I think.
 		in_party = 6,		// Guess, appears to be correct
 		paralyzed = 7,
 		poisoned = 8,
