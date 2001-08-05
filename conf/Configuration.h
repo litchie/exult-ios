a2f204842fe0aee82fe60f24391ccb46d095afbd
Max Horn <max@quendi.de>
2001-08-05 19:18:04 +0000

    * files/utils.cc/.h: added missing prototypes* conf/XMLEntity.c/.h: removed class XMLEntity; added XMLNode methods dump, xmlassign and xmlparse* conf/xmldump.cc, conf/xmlparse.cc: removed* conf/Configuration.cc: make use of new XMLNode methods dump/xmlassign/xmlparse* exult_constants.h: added c_empty_string constant* desktop/exult.png: new picture* mac/*: updated/added various mac specific files* various files: updated legal headers

--- conf/Configuration.h
+++ conf/Configuration.h
@@ -1,30 +1,23 @@
 /*
-Copyright (C) 2000  Dancer A.L Vesperman
+ *  Copyright (C) 2000-2001  The Exult Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
-
-
-//-*-Mode: C++;-*-
 #ifndef _Configuration_h_
 #define _Configuration_h_
-
-#if __GNUG__ >= 2
-#  pragma interface
-#endif
-
 
 
 #include "XMLEntity.h"
@@ -32,9 +25,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 class	Configuration
 {
 public:
-	Configuration();
-	Configuration(const char *);
-	~Configuration();
+	Configuration() : xmltree("config"),filename(),is_file(false)
+		{ }
+	Configuration(const char *s) : xmltree("config"),filename(),is_file(false)
+		{ read_config_file(s); }
 
 	bool	read_config_file(const char *);
 	bool	read_config_string(const std::string &);
@@ -46,7 +40,7 @@ public:
 
 	// Return a list of keys that are subsidiary to the supplied
 	// key
-	std::vector<std::string>	listkeys(std::string &key,bool longformat=true);
+	std::vector<std::string>	listkeys(const std::string &key,bool longformat=true);
 	std::vector<std::string>	listkeys(const char *key,bool longformat=true);
 
 	std::string	dump(void); // Assembles a readable representation
@@ -55,13 +49,12 @@ public:
 
 
 private:
-	std::string	&value(const char *key,bool &exists);
+	const std::string	&value(const char *key,bool &exists);
 	void    set(std::string &key,std::string &value,bool write_to_file);
 	XMLnode xmltree;
 	std::string	filename;
 	bool	is_file;
 };
-
 
 
 #endif
--- conf/XMLEntity.h
+++ conf/XMLEntity.h
@@ -1,62 +1,55 @@
 /*
-Copyright (C) 2000  Dancer A.L Vesperman
+ *  Copyright (C) 2000-2001  The Exult Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
-
-
-//-*-Mode: C++;-*-
-#ifndef _XMLEntity_h_
-#define _XMLEntity_h_
-
-
-#if __GNUG__ >= 2
-#  pragma interface
-#endif
-
-
+#ifndef XMLENTITY_H
+#define XMLENTITY_H
 
 #include <string>
 #include <vector>
 
 
-class	XMLEntity
-	{
-public:
-	std::string	id;
-	std::string	content;
-	XMLEntity &operator =(const XMLEntity &x) { id=x.id; content=x.content; return *this; }
-	XMLEntity(const XMLEntity &e) : id(e.id),content(e.content)
-		{  }
-	XMLEntity();
-	~XMLEntity();
-	};
-
-
 class	XMLnode
-	{
-public:
-	XMLEntity	entity;
+{
+protected:
+	std::string				id;
+	std::string				content;
 	std::vector<XMLnode*>	nodelist;
-	std::string	&reference(std::string &,bool &);
-	XMLnode *subtree(std::string &);
-	XMLnode &operator=(const XMLnode &n) { entity=n.entity; nodelist=n.nodelist; return *this; }
-	XMLnode(const XMLnode &n) : entity(n.entity),nodelist(n.nodelist)
+
+public:
+	XMLnode()
 		{  }
-	XMLnode();
-	~XMLnode();
-	};
+	XMLnode(const std::string &i) : id(i)
+		{  }
+	XMLnode(const XMLnode &n) : id(n.id),content(n.content),nodelist(n.nodelist)
+		{  }
+
+	XMLnode &operator=(const XMLnode &n) { id=n.id; content=n.content; nodelist=n.nodelist; return *this; }
+	const std::string &reference(const std::string &,bool &);
+	XMLnode *subtree(const std::string &);
+	
+	const std::string &value(void)
+		{ return content; }
+	
+	std::string dump(int depth = 0);
+	void	xmlassign(std::string &key,std::string &value);
+	void	xmlparse(std::string &s,std::size_t &pos);
+
+	void	listkeys(const std::string &,std::vector<std::string> &, bool longformat=true);
+};
 
 #endif
