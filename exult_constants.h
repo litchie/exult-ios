5e844bcf0604868dbd75d48b1228998ab9850e2d
fingolfin <fingolfin@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-08-06 12:09:57 +0000

    * exult_constants.h: added FORGET_OBJECT and FORGET_ARRAY

--- exult_constants.h
+++ exult_constants.h
@@ -76,4 +76,11 @@ extern const std::string c_empty_string;
 #  define CERR(x)		
 #endif
 
+// Two very useful macros that one should use instead of pure delete; they will additionally
+// set the old object pointer to 0, thus helping prevent double deletes (not that "delete 0"
+// is a no-op.
+#define FORGET_OBJECT(x) do { delete x; x = 0; } while(0)
+#define FORGET_ARRAY(x) do { delete [] x; x = 0; } while(0)
+
+
 #endif
