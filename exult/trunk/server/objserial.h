73ec58dc01da89261667bf91ca10083b87f6fafd
nadir <nadir@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-08-07 13:33:36 +0000

    2001-08-07  Tristan Tarrant <nadir@users.sourceforge.net> 	* server/objserial.h: add an #include <string> 	* imagewin/scale.cc: copyright of 2xSaI is GPL

--- exult/trunk/server/objserial.h
+++ exult/trunk/server/objserial.h
@@ -25,6 +25,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef OBJSERIAL_H
 #define OBJSERIAL_H	1
 
+#include <string>
+
 extern int Object_out
 	(
 	int fd,				// Socket.
