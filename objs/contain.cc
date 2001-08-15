d23ba8248698d43ebd00b4cc67645b1fb673f4d4
Max Horn <max@quendi.de>
2001-08-15 22:37:23 +0000

    made compile on OS X

--- objs/contain.cc
+++ objs/contain.cc
@@ -31,7 +31,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "objiter.h"
 #include "game.h"
 #include "ucmachine.h"
-#include "files/utils.h"
+#include "utils.h"
 #include "Gump_manager.h"
 
 using std::rand;
