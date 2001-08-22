481ab3f486e4f094876366f7a5af4e9a6b8ee287
Max Horn <max@quendi.de>
2001-08-22 17:59:33 +0000

    * audio/midi_drivers/mac_midi.*: changed copyright back to "Max Horn" with consent of the team. This was done so that the code can be used for SDL_mixer, too.

--- audio/midi_drivers/mac_midi.cc
+++ audio/midi_drivers/mac_midi.cc
@@ -1,7 +1,7 @@
 /*
  *	mac_midi.cc - QuickTime based midi player for MacOS.
  *
- *  Copyright (C) 2000-2001  The Exult Team
+ *  Copyright (C) 2001  Max Horn
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
