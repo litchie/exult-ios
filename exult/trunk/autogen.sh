558ab62cd1db6dff52087e2983eba8e3f2e639cf
fingolfin <fingolfin@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-09-04 19:50:43 +0000

    moved generated autoconf files into CVS, should be usable for most

--- exult/trunk/autogen.sh
+++ exult/trunk/autogen.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 # First of all clean up the generated crud
-rm -f configure config.log config.guess config.sub config.cache
-rm -f libtool ltmain.sh missing mkinstalldirs install-sh
-rm -f autoconfig.h.in
-rm -f config.status aclocal.m4
-rm -f `find . -name 'Makefile.in'`
-rm -f `find . -name 'Makefile'`
+#rm -f configure config.log config.guess config.sub config.cache
+#rm -f libtool ltmain.sh missing mkinstalldirs install-sh
+#rm -f autoconfig.h.in
+#rm -f config.status aclocal.m4
+#rm -f `find . -name 'Makefile.in'`
+#rm -f `find . -name 'Makefile'`
+
+# touch the configure.in file to force rebuilding configure
+touch configure.in
 
 # Regenerate everything
-libtoolize --force --copy
 aclocal
+libtoolize --force --copy
 autoheader
 automake --add-missing --gnu
 autoconf 
+
+echo "You are now ready to run ./configure"
