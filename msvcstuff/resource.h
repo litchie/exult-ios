9919ea473bddfb23325ac01fb9861d7d56e921d3
colourles <colourles@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-09-21 17:54:08 +0000

    * msvcstuff/.cvsignore : Fixed some problems added more files 	* msvcstuff/Exult.dsw : Forgot to re-add this ages ago. 	* msvcstuff/exconfig/* : Files for exconfig helper dll used with the 	  new windows installer.

---
+++ msvcstuff/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Exult.rc
+//
+#define IDI_ICON1                       101
+#define IDD_DIALOG1                     103
+#define IDC_BGPATH                      1000
+#define IDC_BGBROWSE                    1001
+#define IDC_SIPATH                      1002
+#define IDC_SIBROWSE                    1003
+#define IDC_SITITLE                     1015
+#define IDC_BGTITLE                     1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
