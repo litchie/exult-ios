1a868179c7948f853e15320218ee2ed63f26018b
nadir <nadir@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-11-12 17:24:51 +0000

    Removed these generated files, as they are more trouble than what it's worth... ltconfig is infact obsolete starting with libtool 1.3d, but acinclude.m4 was still referencing it... I have started making some modifications to autogen.sh so that some checking is performed. We need to check for actual versions in there, but my scripting is a bit rusty. I guess:
    autoconf 2.13 or later
    automake 1.4 or later
    libtool 1.4 or later

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,43 @@
 #!/bin/sh
-# First of all clean up the generated crud
-#rm -f configure config.log config.guess config.sub config.cache
-#rm -f libtool ltmain.sh missing mkinstalldirs install-sh
-#rm -f autoconfig.h.in
-#rm -f config.status aclocal.m4
-#rm -f `find . -name 'Makefile.in'`
-#rm -f `find . -name 'Makefile'`
+
+# Check for availability
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have 'autoconf' installed to compile $PKG_NAME."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \utomake' installed to compile $PKG_NAME."
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.4.tar.gz"
+  echo "(or a newer version if it is available)"
+  DIE=1
+  NO_AUTOMAKE=yes
+}
+# if no automake, don't bother testing for aclocal
+test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: Missing 'aclocal'.  The version of 'automake'"
+  echo "installed doesn't appear recent enough."
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.4.tar.gz"
+  echo "(or a newer version if it is available)"
+  DIE=1
+}
+
+if test "$DIE" -eq 1; then
+  exit 1
+fi
+
+# Clean up the generated crud
+rm -f configure config.log config.guess config.sub config.cache
+rm -f libtool ltmain.sh missing mkinstalldirs install-sh
+rm -f autoconfig.h.in
+rm -f config.status aclocal.m4
+rm -f `find . -name 'Makefile.in'`
+rm -f `find . -name 'Makefile'`
 
 # touch the configure.in file to force rebuilding configure
 touch configure.in
