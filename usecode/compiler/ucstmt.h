7fb32438edc4aeb0ea81178478d8120808837007
dancer <dancer@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2001-12-03 00:07:43 +0000

    * Fix broken compiler compilation, by inserting some using std::vector statements.

--- usecode/compiler/ucstmt.h
+++ usecode/compiler/ucstmt.h
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define INCL_UCSTMT
 
 #include <vector>
+using std::vector;
 #include "ucloc.h"
 
 class Uc_expression;
