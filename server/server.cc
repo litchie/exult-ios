96a7746d485a46341807723a8139b7dc5dc5a6d5
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2001-12-29 00:39:16 +0000

    * server.cc, gumps_utils.h: some minor changes for AIX 	* shapeinf.h: removed static var from inlined function

--- server/server.cc
+++ server/server.cc
@@ -46,7 +46,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/socket.h>
 #endif
 
-#include <stdio.h>
+#include <cstdio>
+
+#ifdef _AIX
+#include <strings.h>
+#endif
 
 #if HAVE_NETDB_H
 #include <netdb.h>
