d233d905a79257637df1085e21f45128e25ba936
Jeff Freedman <jsf@users.sourceforge.net>
2002-01-04 23:46:49 +0000

    Bug fix

--- objs/jawbone.h
+++ objs/jawbone.h
@@ -28,7 +28,7 @@ class Jawbone_object : public Container_game_object {
 	Jawbone_object(int shapenum, int framenum, unsigned int tilex, 
 				unsigned int tiley, unsigned int lft,
 				char res = 0)
-		: Container_game_object(shapenum, framenum, tilex, tiley, lift, res)
+		: Container_game_object(shapenum, framenum, tilex, tiley, lft, res)
 		{ }
 	Jawbone_object() : Container_game_object() {  }
 
