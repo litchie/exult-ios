57a39279c2eaac4789289bb1073563cf9e31ce1f
takhisis <takhisis@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-01-16 11:30:37 +0000

    * shapes/monstinf.h: Added missing #include <cassert>. * conf/*.cc: Fixed 'special character' (&amp; etc.) encoding/decoding.

--- exult/trunk/shapes/monstinf.h
+++ exult/trunk/shapes/monstinf.h
@@ -29,6 +29,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #  include <iosfwd>
 #endif
 
+#include <cassert>
+
 /*
  *	An element from 'equip.dat', describing a monster's equipment:
  */
