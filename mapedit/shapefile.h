dee4efeffdf26beccbc1947914f95906820f843a
Jeff Freedman <jsf@users.sourceforge.net>
2002-01-24 07:40:24 +0000

    Struct. to hold shape file info

---
+++ mapedit/shapefile.h
@@ -0,0 +1,70 @@
+/**
+ **	Information about a shapes file.
+ **
+ **	Written: 1/23/02 - JSF
+ **/
+
+#ifndef INCL_SHAPEFILE
+#define INCL_SHAPEFILE	1
+
+/*
+Copyright (C) 2002 The Exult Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <string>
+#include <vector>
+
+class Vga_file;
+class Shape_group_file;
+
+/*
+ *	A shape file:
+ */
+class Shape_file_info
+	{
+	string pathname;		// Full pathname.
+	Vga_file *ifile;		// Contains the images.
+	Shape_group_file *groups;	// Groups within ifile.
+public:
+	friend class Shape_file_set;
+					// We will own ifile and groups.
+	Shape_file_info(const char *nm, Vga_file *i, Shape_group_file *g)
+		: pathname(nm), ifile(i), groups(g)
+		{  }
+	~Shape_file_info();
+	const char *get_pathname()
+		{ return pathname.c_str(); }
+	Vga_file *get_ifile()
+		{ return ifile; }
+	Shape_group_file *get_groups()
+		{ return groups; }
+	};
+
+/*
+ *	A set of Shape_file's.
+ */
+class Shape_file_set
+	{
+	vector<Shape_file_info *> files;
+public:
+	Shape_file_set() {  }
+	~Shape_file_set();
+					// Create, or return existing one.
+	Shape_file_info *create(const char *basename, const char *fullname);
+	};
+
+#endif
