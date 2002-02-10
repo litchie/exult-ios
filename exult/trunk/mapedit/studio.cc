263a36b9ae7cec74de724af1f56056bc5bae6782
takhisis <takhisis@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-02-10 00:53:40 +0000

    * audio/xmidi.cc: Fixed const. * mapedit/studio.cc: Fixed namespaces.

--- exult/trunk/mapedit/studio.cc
+++ exult/trunk/mapedit/studio.cc
@@ -61,6 +61,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 using std::cerr;
 using std::cout;
 using std::endl;
+using std::string;
 
 ExultStudio *ExultStudio::self = 0;
 
