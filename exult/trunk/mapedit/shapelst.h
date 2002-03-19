9c0d3c90eb009fb19a75c5765a6b206122c3c812
kirben <kirben@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-03-19 08:43:12 +0000

    Exult Studio compile fixes (Thanks to wjp and Darke) and added make toolsdist option to makefile for snapshots.

--- exult/trunk/mapedit/shapelst.h
+++ exult/trunk/mapedit/shapelst.h
@@ -29,6 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "shapedraw.h"
 #include "rect.h"
 #include <vector>
+#include <ctime>
 
 class Vga_file;
 class Image_buffer8;
