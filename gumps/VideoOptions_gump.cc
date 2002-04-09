75587668605fb7513b645e6cdb77719c8ccf6a41
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-04-09 18:20:28 +0000

    * VideoOptions_gump.cc: minor change for Solaris

--- gumps/VideoOptions_gump.cc
+++ gumps/VideoOptions_gump.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2001  The Exult Team
+ *  Copyright (C) 2001-2002  The Exult Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 
 #include "SDL_events.h"
 
