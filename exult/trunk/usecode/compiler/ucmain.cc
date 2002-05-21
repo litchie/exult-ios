dd6a430eab26970020c8a9670976ff05b644c057
fingolfin <fingolfin@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-05-21 21:56:19 +0000

    some fixes to get exult to compile over here again

--- exult/trunk/usecode/compiler/ucmain.cc
+++ exult/trunk/usecode/compiler/ucmain.cc
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #include <fstream.h>
-#include <getopt.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string>
 #include <vector>
