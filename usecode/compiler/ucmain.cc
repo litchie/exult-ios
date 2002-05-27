16db3f45118794838afe0fc7e4965f1271ccc042
Travis Howell <kirben@users.sourceforge.net>
2002-05-27 04:12:19 +0000

    Add island patch option to makefiles, add win32 temp fix to ucc

--- usecode/compiler/ucmain.cc
+++ usecode/compiler/ucmain.cc
@@ -34,6 +34,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ucloc.h"
 #include "ucfun.h"
 
+#ifdef WIN32
+#include <getopt.h>
+#endif
+
 #ifndef ALPHA_LINUX_CXX
 #  include <iosfwd>
 #endif
