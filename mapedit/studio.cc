6732593a742213a97b57009dcfdd8b4ac32f33bd
kirben <kirben@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-06-05 15:45:00 +0000

    Remove doubles and correct island name

--- mapedit/studio.cc
+++ mapedit/studio.cc
@@ -36,7 +36,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifdef WIN32
 #include <getopt.h>
 #include <windows.h>
-#include <getopt.h>
 #include <ole2.h>
 #include "servewin32.h"
 #else
