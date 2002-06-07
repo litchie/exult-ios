6062e81ec1b511baf5fc4bf1fbadc3f55f3a7a61
Jeff Freedman <jsf@users.sourceforge.net>
2002-06-07 04:39:12 +0000

    Better dropping

--- drag.h
+++ drag.h
@@ -27,11 +27,13 @@
 class Dragging_info
 	{
 	Game_object *obj;		// What's being dragged.
+	bool is_new;			// Object was newly created.
 	Gump *gump;
 	Gump_button *button;
 	Tile_coord old_pos;		// Original pos. of object if it wasn't
 					//   in a container.
 	Rectangle old_foot;		// Original footprint.
+	int old_lift;			// Lift of obj OR its owner.
 	int quantity;			// Amount of object being moved.
 	int readied_index;		// If it was a 'readied' item.
 					// Last mouse, paint positions:
@@ -39,17 +41,22 @@ class Dragging_info
 	Rectangle rect;			// Rectangle to repaint.
 	Image_buffer *save;		// Image below dragged object.
 	bool okay;			// True if drag constructed okay.
+	bool possible_theft;		// Moved enough to be 'theft'.
 
 	bool start(int x, int y);	// First motion.
 	void put_back();		// Put back object.
+	bool drop_on_gump(int x, int y, Game_object *to_drop, Gump *gump);
+	bool drop_on_map(int x, int y, Game_object *to_drop);
 	bool drop(int x, int y);	// Drop obj. at given position.
 public:
 	friend class Game_window;
+					// Create for dropping new object.
+	Dragging_info(Game_object *newobj);
 	Dragging_info(int x, int y);	// Create for given mouse position.
 	~Dragging_info();
 	bool moved(int x, int y);	// Mouse moved.
 					// Mouse button released.
-	bool mouse_up(int x, int y, bool moved);
+	bool drop(int x, int y, bool moved);
 	};
 
 #endif	/* INCL_DRAG_H */
