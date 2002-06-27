3f8bef61bb956b90255c56b7b9a4bf0b234fe69d
Max Horn <max@quendi.de>
2002-06-27 12:48:28 +0000

    Fixed compilation with CodeWarrior 7 (MacOS Classic)

--- objs/objiter.cc
+++ objs/objiter.cc
@@ -41,7 +41,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 template<class D> 
-D_Recursive_object_iterator<D>::D_Recursive_object_iterator<D>
+D_Recursive_object_iterator<D>::D_Recursive_object_iterator
 	(
 	Game_object *start		// Start here.
 	) : elems(Game_window::get_instance()->get_chunk(
