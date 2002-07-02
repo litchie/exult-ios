759fc3945ea33ddce0eb4f26c81e9a9360368cb9
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-07-02 18:35:13 +0000

    * objs.cc: fixes to SI item names (thanks sb-x)

--- objs/objs.cc
+++ objs/objs.cc
@@ -984,7 +984,7 @@ string Game_object::get_name
 
 	} else if (Game::get_game_type() == SERPENT_ISLE) {
 		//TODO: yourself, misc. checks/fixes
-		
+
 		switch (shnum)			// More special cases!
 		{
 		case 0x097:         // wall
@@ -1105,7 +1105,6 @@ string Game_object::get_name
 			}
 			break;
 		case 0x172:         // floor
-			// TODO: CHECK!
 			if (frnum == 19)
 				name = item_names[0x60e]; // wall
 			else
@@ -1151,7 +1150,6 @@ string Game_object::get_name
 			name = item_names[0x581 + frnum/2];
 			break;
 		case 0x1db:         // wall of lights
-			// TODO: CHECK
 			if (frnum == 10)
 				name = item_names[0x683]; // wall
 			else
@@ -1161,7 +1159,6 @@ string Game_object::get_name
 			name = item_names[0x5f8 + frnum/2];
 			break;
 		case 0x1e3:         // rug
-			// TODO: CHECK
 			if (frnum >= 2)
 				name = item_names[0x608]; // meditation mat
 			else
@@ -1201,7 +1198,6 @@ string Game_object::get_name
 			name = item_names[0x5a3 + frnum];
 			break;
 		case 0x220:         // broken column
-			// TODO: CHECK
 			if (frnum == 14)
 				name = item_names[0x611]; // pillar of purity
 			else
@@ -1307,15 +1303,20 @@ string Game_object::get_name
 				name = item_names[shnum];
 			break;
 		case 0x2b5:        // whistle
-			// TODO: CHECK
 			if (frnum == 4 || frnum == 5)
-				name = item_names[0x618]; // bone whistle
+				name = item_names[0x618]; // bone flute
 			else
 				name = item_names[shnum];
 			break;
 		case 0x2b8:        // bed
 			if (frnum == 1)
 				name = item_names[0x5da]; // stone bier
+			else if (frnum == 2)
+				name = item_names[0x5db]; // cot
+			else if (frnum == 4)
+				name = item_names[0x5dc]; // pallet
+			else if (frnum == 5)
+				name = item_names[0x5dd]; // fur pallet
 			else
 				name = item_names[shnum];
 			break;
@@ -1332,15 +1333,12 @@ string Game_object::get_name
 				name = item_names[shnum];
 			break;
 		case 0x2d7:        // Dream Crystal
-			// TODO: CHECK
-			if (frnum == 1)
+			if (frnum <= 1)
 				name = item_names[0x659]; // dream crystal
 			else if (frnum == 2)
 				name = item_names[0x65a]; // mirror rock
 			else if (frnum >= 3)
 				name = item_names[0x65b]; // icy column
-			else
-				name = item_names[shnum];
 			break;
 		case 0x2d8:        // Force_Wall
 			// TODO: CHECK
@@ -1358,7 +1356,7 @@ string Game_object::get_name
 		case 0x313:        // lever
 			if (frnum == 8 || frnum == 9)
 				name = item_names[0x613]; // button
-			else
+			else		
 				name = item_names[shnum];
 			break;
 		case 0x314:        // switch
