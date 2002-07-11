ef58855f0aae1e87330d263aaacd939f75864907
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-07-11 01:23:58 +0000

    Fixed pathfinding problem

--- exult/trunk/objs/chunks.h
+++ exult/trunk/objs/chunks.h
@@ -95,7 +95,7 @@ class Chunk_cache : public Game_singletons
 	int get_lowest_blocked(int lift, int tx, int ty);
 					// Is a spot occupied?
 	int is_blocked(int height, int lift, int tx, int ty, int& new_lift,
-				const int move_flags, int max_drop = 1);
+		const int move_flags, int max_drop = 1, int max_rise = -1);
 					// Activate eggs nearby.
 	void activate_eggs(Game_object *obj, Map_chunk *chunk, 
 			int tx, int ty, int tz, int from_tx, int from_ty, 
@@ -198,20 +198,21 @@ public:
 		{ return need_cache()->get_lowest_blocked(lift, tx, ty); }
 					// Is a spot occupied?
 	int is_blocked(int height, int lift, int tx, int ty, int& new_lift,
-					const int move_flags, int max_drop = 1)
+		const int move_flags, int max_drop = 1, int max_rise = -1)
 		{ return cache->is_blocked(height, lift, tx, ty, new_lift,
-						move_flags, max_drop); }
+					move_flags, max_drop, max_rise); }
 					// Check range.
 	static int is_blocked(int height, int lift, int startx, int starty,
 		int xtiles, int ytiles, int& new_lift, const int move_flags, 
-							int max_drop = 1);
+					int max_drop, int max_rise = -1);
 					// Check absolute tile.
 	static int is_blocked(Tile_coord& tile, int height = 1,
-		const int move_flags = MOVE_ALL_TERRAIN, int max_drop = 1);
+		const int move_flags = MOVE_ALL_TERRAIN, 
+				int max_drop = 1, int max_rise = -1);
 					// Check for > 1x1 object.
 	static int is_blocked(int xtiles, int ytiles, int ztiles,
 			Tile_coord from, Tile_coord& to, const int move_flags,
-							int max_drop = 1);
+					int max_drop = 1, int max_rise = -1);
 	enum Find_spot_where		// For find_spot() below.
 		{
 		anywhere = 0,
