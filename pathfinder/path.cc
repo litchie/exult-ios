d255708d9e672cb608ab8a41181d517f5f7fcc0e
Ryan Nunn <colourles@users.sourceforge.net>
2002-07-28 05:56:36 +0000

    * pathfinder/path.cc : Fixed seriously broken non hash pathfinding (z 	  coord was being ignored in comparisons of nodes). This fixes the 	  Monitor Training problem. Probably fixes countless other pathfinding 	  problems too.

--- pathfinder/path.cc
+++ pathfinder/path.cc
@@ -238,7 +238,7 @@ public:
 	size_t operator() (const Search_node *a) const
 		{
 		const Tile_coord t = a->get_tile();
-		return ((t.tz << 16) + (t.ty << 8) + t.tx);
+		return ((t.tz << 24) + (t.ty << 12) + t.tx);
 		}
 	};
 
@@ -267,8 +267,10 @@ public:
      		{
 			Tile_coord ta = a->get_tile(), tb = b->get_tile();
 			int apos = ta.tx << 16, bpos = tb.tx << 16;
-			apos |= ta.ty;
-			bpos |= tb.ty;
+			apos |= ta.ty << 4;
+			bpos |= tb.ty << 4;
+			apos |= ta.tz;
+			bpos |= tb.tz;
 			/* Because #(short x short) is <= #int, we can define an injective projection,
 			** which is all we need. */
 			return apos < bpos;
