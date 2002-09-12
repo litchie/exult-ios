0ada08c0313266c61b60f2e0ad64b8b6009310b0
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-09-12 17:35:47 +0000

    UseFood

--- exult/trunk/gamewin.h
+++ exult/trunk/gamewin.h
@@ -456,7 +456,7 @@ public:
 	inline void set_allow_double_right_move(bool a) { allow_double_right_move = a; }
 	inline bool get_allow_double_right_move() { return allow_double_right_move; }
 	void teleport_party(Tile_coord t, bool skip_eggs = false);
-	void activate_item(int shnum, int frnum=c_any_framenum,
+	bool activate_item(int shnum, int frnum=c_any_framenum,
 			   int qual=c_any_qual); // Activate item in party.
 					// Find object (x, y) is in.
 	Game_object *find_object(int x, int y);
