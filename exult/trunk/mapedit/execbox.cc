2bffb3512154719133e90a1394d4715e58c583d1
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-11-03 17:32:32 +0000

    * mapedit/execbox.cc: #include <iostream>

--- exult/trunk/mapedit/execbox.cc
+++ exult/trunk/mapedit/execbox.cc
@@ -29,8 +29,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
-#include <iostream.h>	/* Debugging only */
+#include <iostream>	/* Debugging only */
 #include <string>
+
+using std::cout;
+using std::endl;
 
 /*
  *	Create.
