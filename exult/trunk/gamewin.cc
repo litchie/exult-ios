be66d8d64a0752283f1b7e8aeec801492963388b
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2003-04-26 20:45:41 +0000

    Improvements in formation-walking

--- exult/trunk/gamewin.cc
+++ exult/trunk/gamewin.cc
@@ -1712,9 +1712,9 @@ void Game_window::stop_actor
 	else
 		{
 		main_actor->stop();	// Stop and set resting state.
-//		paint();	// ++++++Necessary?
 		if (!gump_man->gump_mode() ||gump_man->gumps_dont_pause_game())
-//			if (!walk_in_formation)	// FOR NOW.
+//+++++++The following line is for testing:
+//			if (!walk_in_formation)
 				main_actor->get_followers();
 		}
 	}
