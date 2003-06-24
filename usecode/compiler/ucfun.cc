ffd66d36d41e8d9383f8c37e9ecfb8285cf1a8f9
fingolfin <fingolfin@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2003-06-24 17:20:56 +0000

    added #include <cassert> to please GCC 3.3

--- usecode/compiler/ucfun.cc
+++ usecode/compiler/ucfun.cc
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #include <stdio.h>
+#include <cassert>
 #include "ucfun.h"
 #include "ucstmt.h"
 #include "utils.h"
