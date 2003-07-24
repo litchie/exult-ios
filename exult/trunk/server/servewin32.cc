bd5010d69a06b65ba0bfdb43988b06fb93a9ee3c
colourles <colourles@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2003-07-24 20:25:02 +0000

    * WinNT and Win2K are forced to use the Win9x style IPC that i wrote. 	  This should now meant that Exult Studio will now work.

--- exult/trunk/server/servewin32.cc
+++ exult/trunk/server/servewin32.cc
@@ -113,7 +113,9 @@ static void detect_winnt()
 	GetVersionEx (&info);
 
 	// Platform is NT
-	if (info.dwPlatformId == 2) bWinNT = TRUE;
+	if (info.dwPlatformId == 2 && (info.dwMajorVersion > 5 ||
+			(info.dwMajorVersion == 5 && info.dwMinorVersion >= 1)))
+		bWinNT = TRUE;
 }
 
 
