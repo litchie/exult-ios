ea1f7ecdbedb603af3ee4542a6630ed2f3901aaa
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2003-08-16 06:39:24 +0000

    EStudio original-game support

--- mouse.cc
+++ mouse.cc
@@ -180,7 +180,7 @@ void Mouse::set_shape0
 	cur_framenum = framenum;
 	cur = pointers.get_frame(framenum); 
 	while (!cur)			// For newly-created games.
-		cur = pointers.get_frame(--cur_framenum);
+		cur = pointers.get_frame(--framenum);
 					// Set backup box to cover mouse.
 	box.x = mousex - cur->get_xleft();
 	box.y = mousey - cur->get_yabove();
