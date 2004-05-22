9d7d6a6f580ff3e166f231b982cd2c3904d7f019
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2004-05-22 22:26:58 +0000

    * gumps/Face_stats.cc: fix crash

--- gumps/Face_stats.cc
+++ gumps/Face_stats.cc
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2001 The Exult Team
+Copyright (C) 2001-2004 The Exult Team
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -89,7 +89,7 @@ void Stat_bar::paint
 {
 	Gump_button::paint();
 
-	int width =  (val * 32) / (max_val);
+	int width =  (val * 32) / (max_val ? max_val : 1);
 
 	if (width > 0)
 	{
