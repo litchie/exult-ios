c9303ea4029a67427f1efe94e45f1b00299fec8f
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2004-12-24 05:42:46 +0000

    Working on notebook rendering

--- gumps/Notebook_gump.cc
+++ gumps/Notebook_gump.cc
@@ -25,8 +25,29 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "game.h"
 // #include "gamewin.h"
 
-vector<One_note> Notebook_gump::notes;
+vector<One_note *> Notebook_gump::notes;
 bool Notebook_gump::initialized = false;	// Set when read in.
+vector<Notebook_top_left> Notebook_gump::page_info;
+
+/*
+ *	setup one note (from already-allocated text).
+ */
+
+void One_note::set
+	(
+	int d, int h, 
+	int la, int ln, 
+	char *txt
+	)
+	{
+	day = d;
+	hour = h;
+	lat = la;
+	lng = ln;
+	delete text;
+	text = txt;
+	textlen = text ? strlen(text) : 0;
+	}
 
 /*
  *	Create notebook gump.
@@ -34,11 +55,17 @@ bool Notebook_gump::initialized = false;	// Set when read in.
 
 Notebook_gump::Notebook_gump
 	(
-	) : Gump(0, EXULT_FLX_NOTEBOOK_SHP, SF_EXULT_FLX),
-		curnote(0), curoff(0)
+	) : Gump(0, EXULT_FLX_NOTEBOOK_SHP, SF_EXULT_FLX), curpage(0)
 	{
 	// +++++++++Guessing obj. area.
 	set_object_area(Rectangle(36, 10, 100, 100), 7, 40);
+	page_info.push_back(Notebook_top_left(0, 0));
+	// ++++TESTING:
+	notes.push_back(new One_note(1, 1, 10, 10, strdup("Note #1\nHello")));
+	notes.push_back(new One_note(2, 2, 20, 20, strdup(
+				"Note #2\nworld.\n\nHow are you?")));
+	notes.push_back(new One_note(3, 3, 30, 30, strdup(
+				"Note #3")));
 	}
 Notebook_gump *Notebook_gump::create
 	(
@@ -49,6 +76,48 @@ Notebook_gump *Notebook_gump::create
 	}
 
 /*
+ *	Paint a page and find where its text ends.
+ *
+ *	Output:	Index past end of displayed page.
+ */
+
+int Notebook_gump::paint_page
+	(
+	Rectangle box,			// Display box rel. to gump.
+	One_note *note,			// Note to print.
+	int start			// Starting offset into text.
+	)
+{
+	const int font = 4;		// Black.
+	const int vlead = 1;		// Extra inter-line spacing.
+	int ypos = 0;
+	int textheight = sman->get_text_height(font) + vlead;
+	char *str = note->text + start;
+	while (*str && ypos + textheight <= box.h)
+		{
+		if (*str == '\n')	// Empty paragraph?
+			{
+			ypos += textheight;
+			str++;
+			continue;
+			}
+		int endoff = sman->paint_text_box(font, str, x + box.x,
+				y + box.y + ypos, box.w, box.h - ypos, vlead);
+		if (endoff > 0)		// All painted?
+			{		// Value returned is height.
+			ypos += endoff;	// ????Need to do this?
+			str = note->text + note->textlen;
+			}
+		else			// Out of room.
+			{
+			str += -endoff;
+			break;
+			}
+		}
+	return (str - note->text);	// Return offset past end.
+}
+
+/*
  *	Paint notebook.
  */
 
@@ -57,6 +126,33 @@ void Notebook_gump::paint
 	)
 {
 	Gump::paint();
-	// +++++FINISH.
-
+	int curnote = page_info[curpage/2].notenum;
+	if (curnote < 0)
+		return;
+	int offset = page_info[curpage/2].offset;
+	One_note *note = notes[curnote];
+					// Paint left page.
+	offset = paint_page(Rectangle(36, 10, 122, 130), note, offset);
+	if (offset >= note->textlen)	// Finished note?
+		{
+		if (curnote == notes.size() - 1)
+			return;
+		++curnote;
+		note = notes[curnote];
+		offset = 0;
+		}
+					// Paint right page.
+	offset = paint_page(Rectangle(174, 10, 122, 130), note, offset);
+	if (offset >= note->textlen)	// Finished note?
+		{
+		if (curnote == notes.size() - 1)
+			return;		// No more.
+		++curnote;
+		offset = 0;
+		}
+	int nxt = curpage/2 + 1;	// For next pair of pages.
+	if (nxt >= page_info.size())
+		page_info.resize(nxt + 1);
+	page_info[nxt].notenum = curnote;
+	page_info[nxt].offset = offset;
 }
