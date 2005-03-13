ababbef93fcfa770f62c4f3381abeb8c462666b3
Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net>
2005-03-13 10:14:42 +0000

    remove confusing DK markers

--- gamewin.cc
+++ gamewin.cc
@@ -490,7 +490,6 @@ void Game_window::init_files(bool cycle)
 	shape_man->load();		// All the .vga files!
 	CYCLE_RED_PLASMA();
 
-	//Setup_text(); //-DK-
 	unsigned long timer = SDL_GetTicks();
 	srand(timer);			// Use time to seed rand. generator.
 					// Force clock to start.
