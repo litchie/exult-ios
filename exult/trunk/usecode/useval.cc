bd0537b1b2ca501596f9c9e59d22e454c56c0077
marzojr <marzojr@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2005-11-19 14:35:04 +0000

    Fixing a bug I introduced when I fixed the Blacksword's 'Fire' power.

--- exult/trunk/usecode/useval.cc
+++ exult/trunk/usecode/useval.cc
@@ -203,9 +203,9 @@ bool Usecode_value::operator==
 					// Okay if 0==empty array.
 			: v2.type == array_type &&
 					// Happens in blacksword usecode:
-					(v2.get_array_size() ?
-					value.intval == v2.get_elem(0).get_int_value() :
-					!value.intval);
+					(v2.get_array_size() && v2.get_elem(0).is_ptr() ?
+					v2.get_elem(0).value.intval == value.intval:
+					!v2.get_array_size() && !value.intval);
 	else if (type == pointer_type)	// Might be ptr==0.
 		{			// Or ptr == array.  Test elem. 0.
 		if (v2.type == pointer_type || v2.type == int_type)
@@ -221,10 +221,10 @@ bool Usecode_value::operator==
 	else if (type == array_type)
 		{
 		if (v2.type == int_type)
-			return get_array_size() ?
-				// Making it simmetric just in case:
-				get_elem(0).get_int_value() == v2.get_int_value() :
-				!v2.get_int_value();
+			// Making it simmetric just in case:
+			return (get_array_size() && get_elem(0).is_ptr() ?
+				get_elem(0).value.intval == v2.value.intval :
+				!get_array_size() && !v2.value.intval);
 		else if (v2.type == pointer_type && get_array_size())
 			{
 			Usecode_value& v = get_elem(0);
