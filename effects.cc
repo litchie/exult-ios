d4d9798074096cd8905ad7d7801fe07749d8ffbe
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2005-12-12 17:32:59 +0000

    very minor cleanup

--- effects.cc
+++ effects.cc
@@ -1,7 +1,7 @@
 /*
- *	effects.h - Special effects.
+ *	effects.cc - Special effects.
  *
- *  Copyright (C) 2000-2001  The Exult Team
+ *  Copyright (C) 2000-2005  The Exult Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
