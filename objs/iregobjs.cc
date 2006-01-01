f18c2da7f51645418e36643f37e8849f6f14709e
Jeff Freedman <jsf@users.sourceforge.net>
2006-01-01 01:14:10 +0000

    Fixed cache-out bug with shapes >=1024

--- objs/iregobjs.cc
+++ objs/iregobjs.cc
@@ -200,6 +200,5 @@ int Ireg_game_object::get_ireg_size()
 	// These shouldn't ever happen, but you never know
 	if (gumpman->find_gump(this) || Usecode_script::find(this))
 		return -1;
-
-	return 11;
+	return 6 + get_common_ireg_size();
 }
