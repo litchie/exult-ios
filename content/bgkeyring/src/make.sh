c2f8e07ba5484ebe4b53268ef2e48f9fb343f6fa
Marzo Sette Torres Junior <marzojr@yahoo.com>
2007-03-31 21:08:15 +0000

    * Added new title screens for Keyring mod and SI Fixes. * Added build scripts/instructions for SI Fixes. Also, improved the Keyring   make scripts. * Added new avatars from Avatar Pack to Keyring mod.

--- content/bgkeyring/src/make.sh
+++ content/bgkeyring/src/make.sh
@@ -7,26 +7,28 @@ if [[ $? -eq 127 ]] ; then
 	exit 127
 fi
 
-ucc -o usecode $0&>/dev/null
-if [[ $? -eq 127 ]] ; then
-	echo "ucc was not found; please add it to your PATH environment variable"
-	echo "and run this script again."
-	exit 127
-fi
-
-for f in faces gumps shapes paperdol sprites fonts
+for f in faces gumps shapes paperdol sprites fonts mainshp
 do
 	if [[ -e "graphics/$f.in" ]] ; then
-		echo "Generating $f.vga..."
+		echo "Generating $f..."
 		expack -i graphics/$f.in
-		rm ../data/$f\_vga.h
+		rm ../data/$f\_*.h
 	fi
 done
 
-echo "Compiling Usecode..."
-ucc -o ../data/usecode usecode.uc
-if [[ $? -eq 0 ]] ; then
-	echo "Usecode has been successfuly compiled!"
-else
-	echo "There were error(s) compiling usecode!"
+if [[ -e "usecode.uc" ]] ; then
+	ucc -o usecode $0&>/dev/null
+	if [[ $? -eq 127 ]] ; then
+		echo "ucc was not found; please add it to your PATH environment variable"
+		echo "and run this script again."
+		exit 127
+	fi
+	
+	echo "Compiling Usecode..."
+	ucc -o ../data/usecode usecode.uc
+	if [[ $? -eq 0 ]] ; then
+		echo "Usecode has been successfuly compiled!"
+	else
+		echo "There were error(s) compiling usecode!"
+	fi
 fi
