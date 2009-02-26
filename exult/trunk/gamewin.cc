df607a38da713f6329bcb6bb62a80e8e9862475e
marzojr <marzojr@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2009-02-26 03:39:43 +0000

    * Fixed bug #2638479. * Allowing NPCs in sleep schedule to do stuff again.

--- exult/trunk/gamewin.cc
+++ exult/trunk/gamewin.cc
@@ -2471,8 +2471,8 @@ void Game_window::schedule_npcs
 		Npc_actor *npc = (Npc_actor *) *it;
 					// Don't want companions leaving.
 		if (npc && npc->get_schedule_type() != Schedule::wait &&
-			(npc->get_schedule_type() == Schedule::combat &&
-				npc->get_target() == 0))
+				(npc->get_schedule_type() != Schedule::combat ||
+					npc->get_target() == 0))
 			npc->update_schedule(hour/3, backwards, hour%3 == 0 ? -1 : 0);
 		}
 
