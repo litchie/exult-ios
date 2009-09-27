034ede33c6cae5cf4b35694ba030e9fd16ed7b80
Dominik Reichardt <dominus@users.sourceforge.net>
2009-09-27 12:24:35 +0000

    CoreAudio builts on OS X 10.6 again

--- audio/midi_drivers/CoreAudioMidiDriver.h
+++ audio/midi_drivers/CoreAudioMidiDriver.h
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <AudioUnit/AudioUnit.h>
 #include <AudioUnit/AUComponent.h>
 #include <CoreMIDI/CoreMIDI.h>
+#include <CoreServices/CoreServices.h>
 
 class CoreAudioMidiDriver : public LowLevelMidiDriver
 {
