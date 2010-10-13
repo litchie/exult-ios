bfa5ed5b3c494ea5991c3027f9aa24d399900028
dominus <dominik.reichardt@gmail.com>
2010-10-13 21:12:40 +0000

    * updated Documentation and FAQ and many other files to reflect an upcoming release of version 1.4.9RC1 * NEWS: updated with what we've done the last six years * AUTHORS: added a couple of authors and changed some email addresses according to our "About Us" page * docs/release.txt: updated a bit

--- msvc9/msvc_kludges.h
+++ msvc9/msvc_kludges.h
@@ -14,7 +14,7 @@
 #define HAVE_SSTREAM 1
 
 // Firstly some things that need to be defined
-#define VERSION "1.4.05svn"
+#define VERSION "1.4.9RC1"
 #define EXULT_DATADIR "data/"
 #define SIZEOF_SHORT 2
 #define SIZEOF_INT 4
