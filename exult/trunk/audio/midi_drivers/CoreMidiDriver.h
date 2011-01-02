7546b6572a9f1a6ea4628cbca385f8733da50148
dominus <dominus@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2011-01-02 13:20:40 +0000

    fixed CoreMidiDriver copyright

--- exult/trunk/audio/midi_drivers/CoreMidiDriver.h
+++ exult/trunk/audio/midi_drivers/CoreMidiDriver.h
@@ -1,5 +1,6 @@
 /*
-Copyright (C) 2003-2005  The Pentagram Team
+Copyright (C) 2009 The ScummVM project
+Copyright (C) 2009-2011  The Exult Team
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
