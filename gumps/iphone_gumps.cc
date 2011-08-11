521182e4a13cd161131350d559f7b25833c5462b
lanica <lanica@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2011-08-11 02:53:48 +0000

    * iPhone: Added shapes to shapeid.cc/.h * iPhone: Started implementation of item menus based upon nearby object detection

--- gumps/iphone_gumps.cc
+++ gumps/iphone_gumps.cc
@@ -33,6 +33,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "shapeid.h"
 #include <string>
 
+#include "Gump_manager.h"
+#include "Gump_button.h"
+#include "exult_flx.h"
+#include "gamewin.h"
+#include "Text_button.h"
+
 using std::string;
 using std::cout;
 using std::endl;
@@ -103,5 +109,169 @@ void KeyboardButton_gump::mouse_up(int mx, int my)
 
 }
 
+class Itemmenu_button : public Text_button {
+public:
+        Itemmenu_button(Gump *par, string text, int px, int py)
+                : Text_button(par, text, px, py, 59, 20)
+                { }
+                                        // What to do when 'clicked':
+        virtual bool activate(int button)
+        {
+                if (button != 1) return false;
+		Itemmenu_gump *par = (Itemmenu_gump *)parent;	
+		if (text == "Cancel")
+		{
+			par->close();
+		}
+		else if (par->objectAction == ITEMMENU_ACTION_NONE)
+		{
+			Gump_button_vector::const_iterator bit = par->buttons.begin();
+			Game_object_vector::const_iterator oit = par->objects.begin();
 
+		        for (;bit < par->buttons.end(); bit++, oit++)
+			{
+			   if (*bit == this)
+			   {
+			      par->objectSelected = ((Game_object *)*oit);
+			      par->close();
+			      break;
+			   }
+			}
+		}
+		else // if (par->objectAction == ITEMMENU_ACTION_MENU
+		{
+			if (text == "Use")
+			{
+			   par->objectAction = ITEMMENU_ACTION_USE;
+			}
+			else if (text == "Cancel")
+			{
+			   par->objectSelected = NULL;
+			}
+			par->close();
+		}
+                return true;
+        }
+};
+
+Itemmenu_gump::Itemmenu_gump(Game_object_vector *vobjs, int cx, int cy) 
+        : Modal_gump(0, cx, cy, EXULT_IPHONE_FLX_TRANSPARENTMENU_SHP, SF_IPHONE_FLX)
+{
+	objectSelected = NULL;
+	objectAction = ITEMMENU_ACTION_NONE;
+	int btop = 0;
+	int byspace = 22;
+        //set_object_area(Rectangle(0, 0, 0, 0), -1, -1);//++++++ ???
+	Itemmenu_button *b;
+	int i = 0;
+	for (Game_object_vector::const_iterator it = vobjs->begin(); it < vobjs->end(); it++, i++)
+	{
+           Game_object *o = *it;
+	   objects.push_back(o);
+	   b = new Itemmenu_button(this, o->get_name().c_str(), 10, btop+(i*byspace));
+	   buttons.push_back((Gump_button *)b);
+	}
+	b = new Itemmenu_button(this, "Cancel", 10, btop+(i*byspace));
+	buttons.push_back((Gump_button *)b);
+}
+
+Itemmenu_gump::Itemmenu_gump(Game_object *obj, int cx, int cy)
+        : Modal_gump(0, cx, cy, EXULT_IPHONE_FLX_TRANSPARENTMENU_SHP, SF_IPHONE_FLX)
+{
+	objectSelected = obj;
+	objectAction = ITEMMENU_ACTION_MENU;
+	int btop = 0;
+	int byspace = 22;
+	Itemmenu_button *b;
+	int i = 0;
+	b = new Itemmenu_button(this, "Use", 10, btop+(i*byspace));
+	buttons.push_back((Gump_button *)b);
+	i++;
+	b = new Itemmenu_button(this, "Cancel", 10, btop+(i*byspace));
+	buttons.push_back((Gump_button *)b);
+}
+
+Itemmenu_gump::~Itemmenu_gump()
+{
+        for (Gump_button_vector::const_iterator it = buttons.begin(); it < buttons.end(); it++)
+	{
+           Gump_button *b = *it;
+	   delete b;
+	}
+}
+	   
+void Itemmenu_gump::paint()
+{
+	Gump::paint();
+        for (Gump_button_vector::const_iterator it = buttons.begin(); it < buttons.end(); it++)
+		(*it)->paint();
+	gwin->set_painted();
+}
+
+bool Itemmenu_gump::mouse_down(int mx, int my, int button)
+{
+        // Only left and right buttons
+        if (button != 1 && button != 3) return false;
+
+        // We'll eat the mouse down if we've already got a button down
+        if (pushed) return true;
+
+        // First try checkmark
+        pushed = Gump::on_button(mx, my);
+                                        
+        // Try buttons at bottom.
+        if (!pushed) {
+        	for (Gump_button_vector::const_iterator it = buttons.begin(); it < buttons.end(); it++)
+		{
+                        if ((*it)->on_button(mx, my)) {
+                                pushed = (Gump_button *)*it;
+                                break;
+                        }
+                }
+        }
+
+        if (pushed && !pushed->push(button))                    // On a button?
+                pushed = 0;
+                
+        return button == 1 || pushed != 0;
+}
+
+bool Itemmenu_gump::mouse_up(int mx, int my, int button)
+{
+        // Not Pushing a button?
+        if (!pushed) return false;
+
+        if (pushed->get_pushed() != button) return button == 1;
+
+        bool res = false;
+        pushed->unpush(button);
+        if (pushed->on_button(mx, my))
+                res = ((Gump_button*)pushed)->activate(button);
+        pushed = 0;
+        return res;
+}
+
+void Itemmenu_gump::postCloseActions()
+{
+	if (!objectSelected)
+		return;
+
+	Itemmenu_gump *itemgump;
+
+	switch (objectAction)
+	{
+	   case ITEMMENU_ACTION_USE:
+		objectSelected->activate();
+		break;
+	   case ITEMMENU_ACTION_NONE:
+		// This will draw a selection menu for the object
+                itemgump = new Itemmenu_gump(objectSelected, x, y);
+                Game_window::get_instance()->get_gump_man()->do_modal_gump(itemgump, Mouse::hand);
+                itemgump->postCloseActions();
+                delete itemgump;
+		break;
+	   case ITEMMENU_ACTION_MENU:
+		break;
+	}	
+}
 #endif
