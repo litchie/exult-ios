ffcbd5df34ecffba762d12d1e99978a75f25e650
Phillip T. George <me@PhillipGeorge.com>
2011-08-11 02:53:48 +0000

    * iPhone: Added shapes to shapeid.cc/.h * iPhone: Started implementation of item menus based upon nearby object detection

--- gumps/iphone_gumps.h
+++ gumps/iphone_gumps.h
@@ -23,6 +23,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "SDL.h"
 #include "gamewin.h"
+#include "../objs/objs.h"
+
+#include "Modal_gump.h"
+#include <string>
+
+using std::string;
 
 extern "C" int SDLCALL SDL_iPhoneKeyboardShow(SDL_Window * window);
 extern "C" int SDLCALL SDL_iPhoneKeyboardHide(SDL_Window * window);
@@ -49,5 +55,33 @@ private:
 	int height;
 };
 
+class Gump_button;
+typedef std::vector<Gump_button *> Gump_button_vector;
+
+enum ITEMMENU_ACTIONS { ITEMMENU_ACTION_NONE, ITEMMENU_ACTION_MENU, ITEMMENU_ACTION_USE, ITEMMENU_ACTION_COUNT };
+class Itemmenu_gump : public Modal_gump
+{
+        UNREPLICATABLE_CLASS_I(Itemmenu_gump,Modal_gump(0,0,0,0));
+ public:
+        Gump_button_vector buttons;
+	Game_object_vector objects;
+	Game_object *objectSelected;
+	int objectAction;
+
+        Itemmenu_gump(Game_object_vector *vobjs, int cx, int cy);
+        Itemmenu_gump(Game_object *obj, int cx, int cy);
+        virtual ~Itemmenu_gump();
+
+                                        // Paint it and its contents.
+        virtual void paint();
+        virtual void close()
+                { done = 1; }
+                                        // Handle events:
+        virtual bool mouse_down(int mx, int my, int button);
+        virtual bool mouse_up(int mx, int my, int button);
+
+	void postCloseActions();
+
+};
 #endif
 #endif
