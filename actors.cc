476340e5ae3e758ff00847e4e546851ad567d242
marzojr <marzojr@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2012-12-30 02:26:19 +0000

    * actors.cc, gamewin.cc, shapes/items.h: Guards now bark messages when   coming to arrest you. People calling guards only yell if they can talk.   If there are no guards in the area, people will call for help instead   of calling guards. Guards are no longer called in dungeons, and people   will not yell for them in such places. This should fix bug #3186715   "SI: Automatons in MoF call guards (1.5.0svn)".

--- actors.cc
+++ actors.cc
@@ -2681,8 +2681,11 @@ void Actor::fight_back
 		long delta = curtime - lastcall;
 		if (delta > 10000)	// Call if 10 secs. has passed.
 			{
-			eman->remove_text_effect(this);
-			say(first_call_police, last_call_police);
+			if (!gwin->is_in_dungeon())
+				{	   // Don't say anything if in a dungeon.
+				eman->remove_text_effect(this);
+				say(first_call_police, last_call_police);
+				}
 			lastcall = curtime;
 			gwin->attack_avatar(1 + rand()%2);
 			}
