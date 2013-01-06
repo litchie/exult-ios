e2b780dc9a5cf5712bb4c9c5901322ff1f643881
Dominik Reichardt <dominus@users.sourceforge.net>
2013-01-06 22:13:39 +0000

    Updated Copyright dates

--- keyactions.cc
+++ keyactions.cc
@@ -204,7 +204,7 @@ void ActionAbout(int const *params)
 	scroll = new Scroll_gump();
 	
 	scroll->add_text("Exult V"VERSION"\n");
-	scroll->add_text("(C) 1998-2011 Exult Team\n\n");
+	scroll->add_text("(C) 1998-2013 Exult Team\n\n");
 	scroll->add_text("Available under the terms of the ");
 	scroll->add_text("GNU General Public License\n\n");
 	scroll->add_text("http://exult.sourceforge.net\n");
