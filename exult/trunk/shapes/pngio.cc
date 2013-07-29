3fc0caa9ae96274595956cb6dc9f07db1f5491cd
kirben <kirben@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2013-07-29 00:47:23 +0000

    Fix tools compile when using latest libpng under mingw.

--- exult/trunk/shapes/pngio.cc
+++ exult/trunk/shapes/pngio.cc
@@ -29,6 +29,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifdef HAVE_PNG_H
 
+#include <cstdio>
+#include <string.h>
+
 #include <png.h>
 #include <setjmp.h>
 
