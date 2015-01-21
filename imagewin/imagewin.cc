818a7f76ac296a0a0df8c1976686ef953f620548
Chaoji Li <lichaoji@gmail.com>
2015-01-21 22:56:34 +0800

    Fix display not updating in iOS device
    For real device, screen stays black even if the display surface is
    being updated correctly. Turns out SDL2  on iOS requires you to pump events
    after render present.

--- imagewin/imagewin.cc
+++ imagewin/imagewin.cc
@@ -662,6 +662,7 @@ bool Image_window::create_scale_surfaces(int w, int h, int bpp) {
 	// Get best bpp
 	flags = SDL_SWSURFACE | (fullscreen ? SDL_FULLSCREEN : 0);
 #ifdef __IPHONEOS__
+	flags |= SDL_WINDOW_BORDERLESS;
 	// Turn on landscape mode if desired
 	if (w > h) {
 		SDL_SetHint(SDL_HINT_ORIENTATIONS, "LandscapeLeft LandscapeRight");
@@ -1346,9 +1347,11 @@ void Image_window::UpdateRect(SDL_Surface *surf, int x, int y, int w, int h)
 	// Seem to get flicker like crazy or some other ill effect no matter
 	// what I try. -Lanica 08/28/2013
 	SDL_UpdateTexture(screen_texture, NULL, surf->pixels, surf->pitch);
-	SDL_Rect destRect = {x, y, w, h};
 	SDL_RenderCopy(screen_renderer, screen_texture, NULL, NULL);
 	SDL_RenderPresent(screen_renderer);
+#ifdef __IPHONEOS__
+	SDL_PumpEvents(); /* Otherwise screen won't be updated */
+#endif
 }
 int Image_window::VideoModeOK(int width, int height, int bpp, Uint32 flags)
 {
