litchie/exult-ios

commit daa282fbee82bea18ef7bbb5343fd384aaf9eb9c

Hide GameControls when a gump is open. The GameControls were often getting in the way.

gumps/Gump_manager.cc
@@ -182,6 +182,10 @@ bool Gump_manager::close_gump(Gump *gump) {
 	if (dragged == gump)
 		gwin->stop_dragging();
 	delete gump;
+#ifdef __IPHONEOS__
+	if (non_persistent_count == 0)
+		touchui->showGameControls();
+#endif
 	return ret;
 }
 
@@ -299,6 +303,9 @@ void Gump_manager::add_gump(
 
 	// Paint new one last.
 	add_gump(new_gump);
+#ifdef __IPHONEOS__
+	touchui->hideGameControls();
+#endif
 	if (++cnt == 8)
 		cnt = 0;
 	int sfx = Audio::game_sfx(14);
@@ -619,6 +626,7 @@ int Gump_manager::do_modal_gump(
 	gkeyboard->paint();
 #endif
 #ifdef __IPHONEOS__
+	touchui->hideGameControls();
 	gkeybb->paint();
 #endif
 	do {
@@ -658,6 +666,10 @@ int Gump_manager::do_modal_gump(
 
 	if (!modal_gump_count)
 		SDL_EnableUNICODE(0);
+#ifdef __IPHONEOS__	
+	if (!modal_gump_count && non_persistent_count == 0)
+		touchui->showGameControls();
+#endif
 
 	return (!escaped);
 }

commit 8498fe2debe123c2c2c443f5e9ef54ad7d5f7d58

Fixed GameControls showing in the Exult Menu after using the setup options from the Exult Menu. Regression from daa282f

gumps/Gump_manager.cc
@@ -667,7 +667,7 @@ int Gump_manager::do_modal_gump(
 	if (!modal_gump_count)
 		SDL_EnableUNICODE(0);
 #ifdef __IPHONEOS__	
-	if (!modal_gump_count && non_persistent_count == 0)
+	if (!modal_gump_count && non_persistent_count == 0 && !gwin->is_in_exult_menu())
 		touchui->showGameControls();
 #endif
 