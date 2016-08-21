litchie/exult-ios

commit 79c4f593a3b4b4b4c038cd97c16fc35b722c3217

Adding SI Beta frame-name remapping. Simplified memory management of everything related to textmsg format or text.flx.

shapes/miscinf.cc
@@ -35,10 +35,12 @@
 #include "actors.h"
 #include "ignore_unused_variable_warning.h"
 
+#include <sstream>
 #include <fstream>
 #include <map>
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using std::vector;
@@ -47,6 +49,8 @@ using std::map;
 using std::multimap;
 using std::pair;
 using std::string;
+using std::stringstream;
+using std::skipws;
 
 using namespace std;
 
@@ -79,22 +83,29 @@ int get_skinvar(std::string key) {
 class Shapeinfo_entry_parser {
 public:
 	virtual ~Shapeinfo_entry_parser() { }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) = 0;
-	int ReadInt(char *&eptr, int off = 1) {
-		int ret = strtol(eptr + off, &eptr, 0);
-		while (isspace(*eptr))
-			eptr++;
+	int ReadInt(istream &src, int off = 1) {
+		src.ignore(off);
+		int ret;
+		if (src.peek() == '0') {
+			src.ignore(1);
+			char chr = src.peek();
+			if (chr == 'x' || chr == 'X') {
+				src.ignore(1);
+				src >> hex;
+			} else {
+				src.unget();
+			}
+		}
+		src >> ret >> skipws >> dec;
 		return ret;
 	}
-	string ReadStr(char *&eptr, int off = 1) {
-		eptr += off;
-		char *pos = strchr(eptr, '/');
-		char buf[50];
-		strncpy(buf, eptr, pos - eptr);
-		buf[pos - eptr] = 0;
-		eptr = pos;
-		return string(buf);
+	string ReadStr(istream &src, int off = 1) {
+		src.ignore(off);
+		string ret;
+		getline(src, ret, '/');
+		return ret;
 	}
 };
 
@@ -104,11 +115,11 @@ public:
 	Int_pair_parser(map<int, int> *tbl)
 		: table(tbl)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
-		int key = ReadInt(eptr, 0);
-		int data = ReadInt(eptr);
+		int key = ReadInt(src, 0);
+		int data = ReadInt(src);
 		(*table)[key] = data;
 	}
 };
@@ -119,10 +130,10 @@ public:
 	Bool_parser(map<int, bool> *tbl)
 		: table(tbl)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
-		int key = ReadInt(eptr, 0);
+		int key = ReadInt(src, 0);
 		(*table)[key] = true;
 	}
 };
@@ -136,23 +147,24 @@ public:
 	(vector<pair<int, int> > *tbl, map<string, int> *sh)
 		: table(tbl), shapevars(sh), shape(c_max_shapes)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
 		pair<int, int> data;
-		data.second = ReadInt(eptr, 0); // The real shape.
+		data.second = ReadInt(src, 0); // The real shape.
 		for (vector<pair<int, int> >::iterator it = table->begin();
 		        it != table->end(); ++it)
 			if ((*it).second == data.second)
 				return;     // Do nothing for repeated entries.
-		eptr++;
-		if (*eptr == '%') {
+		src.ignore(1);
+		if (src.peek() == '%') {
 			data.first = shape;     // The assigned shape.
-			string key(eptr);
+			string key;
+			src >> key;
 			(*shapevars)[key] = shape;
 			shape++;    // Leave it ready for the next shape.
 		} else
-			data.first = ReadInt(eptr, 0);
+			data.first = ReadInt(src, 0);
 		table->push_back(data);
 	}
 };
@@ -165,21 +177,22 @@ public:
 	(vector<pair<int, int> > *tbl, map<string, int> *sh)
 		: table(tbl), shapevars(sh)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
 		pair<int, int> data;
-		data.first = ReadInt(eptr, 0);  // The spot.
-		eptr++;
-		if (*eptr == '%') {
-			string key(eptr);
+		data.first = ReadInt(src, 0);  // The spot.
+		src.ignore(1);
+		if (src.peek() == '%') {
+			string key;
+			src >> key;
 			map<string, int>::iterator it = shapevars->find(key);
 			if (it != shapevars->end())
 				data.second = (*it).second; // The shape #.
 			else
 				return; // Invalid reference; bail out.
 		} else
-			data.second = ReadInt(eptr, 0);
+			data.second = ReadInt(src, 0);
 		table->push_back(data);
 	}
 };
@@ -191,7 +204,7 @@ public:
 	Paperdoll_source_parser(vector<pair<string, int> > *tbl)
 		: table(tbl), erased_for_patch(false)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version);
 };
 
@@ -201,14 +214,14 @@ public:
 	Def_av_shape_parser(map<bool, Base_Avatar_info> *tbl)
 		: table(tbl)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
-		bool fmale = ReadInt(eptr, 0) != 0;
+		bool fmale = ReadInt(src, 0) != 0;
 		Base_Avatar_info entry;
-		entry.shape_num = ReadInt(eptr);
-		entry.face_shape = ReadInt(eptr);
-		entry.face_frame = ReadInt(eptr);
+		entry.shape_num = ReadInt(src);
+		entry.face_shape = ReadInt(src);
+		entry.face_frame = ReadInt(src);
 		(*table)[fmale] = entry;
 	}
 };
@@ -219,11 +232,11 @@ public:
 	Base_av_race_parser(Avatar_default_skin *tbl)
 		: table(tbl)
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
-		table->default_skin = ReadInt(eptr, 0);
-		table->default_female = ReadInt(eptr) != 0;
+		table->default_skin = ReadInt(src, 0);
+		table->default_female = ReadInt(src) != 0;
 	}
 };
 
@@ -234,35 +247,35 @@ public:
 	Multiracial_parser(vector<Skin_data> *tbl, map<string, int> *sh)
 		: table(tbl), shapevars(sh)
 	{  }
-	int ReadVar(char *&eptr) {
-		eptr++;
-		if (*eptr == '%') {
-			string key = ReadStr(eptr, 0);
+	int ReadVar(istream &src) {
+		src.ignore(1);
+		if (src.peek() == '%') {
+			string key = ReadStr(src, 0);
 			map<string, int>::iterator it = shapevars->find(key);
 			if (it != shapevars->end())
 				return (*it).second;    // The var value.
 			else
 				return -1;  // Invalid reference; bail out.
 		} else
-			return ReadInt(eptr, 0);
+			return ReadInt(src, 0);
 	}
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index);
 		Skin_data entry;
-		entry.skin_id = ReadInt(eptr, 0);
+		entry.skin_id = ReadInt(src, 0);
 		if (entry.skin_id > last_skin)
 			last_skin = entry.skin_id;
-		entry.is_female = ReadInt(eptr) != 0;
-		if ((entry.shape_num = ReadVar(eptr)) < 0)
+		entry.is_female = ReadInt(src) != 0;
+		if ((entry.shape_num = ReadVar(src)) < 0)
 			return;
-		if ((entry.naked_shape = ReadVar(eptr)) < 0)
+		if ((entry.naked_shape = ReadVar(src)) < 0)
 			return;
-		entry.face_shape = ReadInt(eptr);
-		entry.face_frame = ReadInt(eptr);
-		entry.alter_face_shape = ReadInt(eptr);
-		entry.alter_face_frame = ReadInt(eptr);
-		entry.copy_info = !(version == 2 && *eptr != 0 && ReadInt(eptr) == 0);
+		entry.face_shape = ReadInt(src);
+		entry.face_frame = ReadInt(src);
+		entry.alter_face_shape = ReadInt(src);
+		entry.alter_face_frame = ReadInt(src);
+		entry.copy_info = !(version == 2 && !src.eof() && ReadInt(src) == 0);
 		if (for_patch && !table->empty()) {
 			unsigned int i;
 			int found = -1;
@@ -288,17 +301,17 @@ public:
 	Avatar_usecode_parser(map<int, Usecode_function_data> *tbl)
 		: table(tbl), usecode(Game_window::get_instance()->get_usecode())
 	{  }
-	virtual void parse_entry(int index, char *eptr,
+	virtual void parse_entry(int index, istream &src,
 	                         bool for_patch, int version) {
 		ignore_unused_variable_warning(index, for_patch, version);
 		Usecode_function_data entry;
-		int type = ReadInt(eptr);
-		if (*eptr == ':') {
-			string name = ReadStr(eptr);
+		int type = ReadInt(src);
+		if (src.peek() == ':') {
+			string name = ReadStr(src);
 			entry.fun_id = usecode->find_function(name.c_str(), true);
 		} else
-			entry.fun_id = ReadInt(eptr);
-		entry.event_id = ReadInt(eptr);
+			entry.fun_id = ReadInt(src);
+		entry.event_id = ReadInt(src);
 		(*table)[type] = entry;
 	}
 };
@@ -306,14 +319,15 @@ public:
 
 void Paperdoll_source_parser::parse_entry(
     int index,
-    char *eptr,
+    istream &src,
     bool for_patch,
     int version
 ) {
 	ignore_unused_variable_warning(index, version);
 	if (!erased_for_patch && for_patch)
 		table->clear();
-	string line(eptr);
+	string line;
+	src >> line;
 	if (line == "static" ||
 	        (GAME_BG && line == "bg") ||
 	        (GAME_SI && line == "si"))
@@ -382,11 +396,10 @@ void Shapeinfo_lookup::Read_data_file(
 	for (unsigned int i = 0; i < static_strings.size(); i++) {
 		Readstrings &section = static_strings[i];
 		for (unsigned int j = 0; j < section.size(); j++) {
-			char *ptr = section[j];
-			if (!ptr)
-				continue;
-			parsers[i]->parse_entry(j, ptr, false, static_version);
-			delete[] section[j];
+			if (section[j].size()) {
+				stringstream src(section[j]);
+				parsers[i]->parse_entry(j, src, false, static_version);
+			}
 		}
 		section.clear();
 	}
@@ -394,11 +407,10 @@ void Shapeinfo_lookup::Read_data_file(
 	for (unsigned int i = 0; i < patch_strings.size(); i++) {
 		Readstrings &section = patch_strings[i];
 		for (unsigned int j = 0; j < section.size(); j++) {
-			char *ptr = section[j];
-			if (!ptr)
-				continue;
-			parsers[i]->parse_entry(j, ptr, true, patch_version);
-			delete[] section[j];
+			if (section[j].size()) {
+				stringstream src(section[j]);
+				parsers[i]->parse_entry(j, src, true, patch_version);
+			}
 		}
 		section.clear();
 	}

exult.cc
@@ -1039,7 +1039,7 @@ static void Init(
 			midi = audio->get_midi();
 		}
 
-		Setup_text(GAME_SI, Game::has_expansion());
+		Setup_text(GAME_SI, Game::has_expansion(), GAME_SIB);
 
 		// Skip splash screen?
 		bool skip_splash;

files/msgfile.cc
@@ -23,6 +23,8 @@
  */
 
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 #include <ctype.h>
 #include <iomanip>
@@ -30,12 +32,14 @@
 #include "utils.h"
 #include "databuf.h"
 
+using std::stringstream;
 using std::istream;
 using std::ostream;
 using std::cerr;
 using std::endl;
 using std::hex;
 using std::vector;
+using std::string;
 
 /*
  *  Read in text, where each line is of the form "nnn:sssss", where nnn is
@@ -53,54 +57,53 @@ using std::vector;
  *      error.
  */
 
-int Read_text_msg_file(DataSource *in, vector<char *> &strings,
+int Read_text_msg_file(DataSource *in, vector<string> &strings,
                        const char *section) {
 	strings.resize(0);      // Initialize.
 	strings.reserve(1000);
-	const char *buf;
 	int linenum = 0;
 #define NONEFOUND 0xffffffff
 	unsigned long first = NONEFOUND;// Index of first one found.
 	long next_index = 0;// For auto-indexing of lines
+	static const string sectionStart("%%section");
+	static const string sectionEnd("%%endsection");
 	while (!in->eof()) {
 		++linenum;
-		std::string s;
-		in->readline(s);
-		if (s.empty())
+		std::string line;
+		in->readline(line);
+		if (line.empty())
 			continue;   // Empty line.
-		buf = s.c_str();
 
-		const char *ptr = &buf[0];
 		if (section) {
-			if (buf[0] != '%' ||
-			        strncmp(ptr + 1, "%section", 8) != 0)
+			if (line.compare(0, sectionStart.length(), sectionStart))
 				continue;
-			for (ptr = &buf[9]; isspace(*ptr); ++ptr)
-				;
-			if (strncmp(ptr, section, strlen(ptr)) == 0) {
+			const string sectionName(line.substr(line.find_first_not_of(" \t\b", sectionStart.length())));
+			if (sectionName == section) {
 				// Found the section.
 				section = 0;
 				continue;
 			}
 			cerr << "Line #" << linenum <<
-			     " has the wrong section name: " << newstrdup(ptr) << " != " << section << endl;
+			     " has the wrong section name: " << sectionName << " != " << section << endl;
 			return -1;
 		}
-		if (buf[0] == '%' && strncmp(ptr + 1, "%endsection", 11) == 0)
+		if (!line.compare(0, sectionEnd.length(), sectionEnd))
 			break;
-		char *endptr;
-		unsigned long index;
 
-		if (buf[0] == ':') {
+		unsigned long index;
+		string lineVal;
+		if (line[0] == ':') {
 			// Auto-index lines missing an index.
 			index = next_index++;
-			endptr = const_cast<char *>(&buf[0]);
+			lineVal = line.substr(1);
+		} else if (line[0] == '#') {
+			continue;
 		} else {
+			char *endptr = &line[0];
+			const char *ptr = endptr;
 			// Get line# in decimal, hex, or oct.
 			index = strtol(ptr, &endptr, 0);
 			if (endptr == ptr) { // No #?
-				if (*ptr == '#')
-					continue;
 				cerr << "Line " << linenum <<
 				     " doesn't start with a number" << endl;
 				return -1;
@@ -110,10 +113,11 @@ int Read_text_msg_file(DataSource *in, vector<char *> &strings,
 				     ".  Ignoring line" << endl;
 				continue;
 			}
+			lineVal = line.substr(endptr - ptr + 1);
 		}
 		if (index >= strings.size())
 			strings.resize(index + 1);
-		strings[index] = newstrdup(endptr + 1);
+		strings[index] = lineVal;
 		if (index < first)
 			first = index;
 	}
@@ -127,23 +131,18 @@ int Read_text_msg_file(DataSource *in, vector<char *> &strings,
  */
 
 bool Search_text_msg_section(DataSource *in, const char *section) {
-	const char *buf;
+	static const string sectionStart("%%section");
 	while (!in->eof()) {
-		std::string s;
+		std::string line;
 		size_t pos = in->getPos();
-		in->readline(s);
-		if (s.empty())
+		in->readline(line);
+		if (line.empty())
 			continue;   // Empty line.
-		buf = s.c_str();
 
-		const char *ptr = &buf[0];
-
-		if (buf[0] != '%' ||
-		        strncmp(ptr + 1, "%section", 8) != 0)
+		if (line.compare(0, sectionStart.length(), sectionStart))
 			continue;
-		for (ptr = &buf[9]; isspace(*ptr); ++ptr)
-			;
-		if (strncmp(ptr, section, strlen(ptr)) == 0) {
+		const string sectionName(line.substr(line.find_first_not_of(" \t\b", sectionStart.length())));
+		if (sectionName == section) {
 			// Found the section.
 			// Seek to just before it.
 			in->seek(pos);
@@ -159,7 +158,7 @@ bool Search_text_msg_section(DataSource *in, const char *section) {
 
 int Read_text_msg_file(
     istream &in,
-    vector<char *> &strings,    // Strings returned here, each
+    vector<string> &strings,    // Strings returned here, each
     //   allocated on heap.
     const char *section         // Section name, or NULL.  If given
     //   the section must be next infile.
@@ -168,44 +167,21 @@ int Read_text_msg_file(
 	return Read_text_msg_file(&ds, strings, section);
 }
 
-/*
- *  Same as above, but store in given list, and set given count.
- */
-
-int Read_text_msg_file(
-    istream &in,
-    char ** &strings,       // Strings returned here, each
-    //   allocated on heap.
-    int &count,
-    const char *section
-) {
-	StreamDataSource ds(&in);
-	vector<char *> txtlist;
-	int first = Read_text_msg_file(&ds, txtlist, section);
-	count = static_cast<int>(txtlist.size());
-	strings = new char *[count];
-	for (int i = 0; i < count; ++i)
-		strings[i] = txtlist[i];
-	return first;
-}
-
 int Read_text_msg_file_sections(
     DataSource *in,
-    vector<vector<char *> > &strings,   // Strings returned here
+    vector<vector<string> > &strings,   // Strings returned here
     const char *sections[],         // Section names
     int numsections
 ) {
 	strings.resize(numsections);
 	int version = 1;
 
-	vector<char *> versioninfo;
+	vector<string> versioninfo;
 	// Read version.
 	const char *versionstr = "version";
 	if (Search_text_msg_section(in, versionstr) &&
 	        Read_text_msg_file(in, versioninfo, versionstr) != -1) {
-		version = static_cast<int>(strtol(versioninfo[0], 0, 0));
-		for (size_t j = 0; j < versioninfo.size(); j++)
-			delete[] versioninfo[j];
+		version = static_cast<int>(strtol(versioninfo[0].c_str(), 0, 0));
 	}
 
 	for (int i = 0; i < numsections; i++) {
@@ -221,7 +197,7 @@ int Read_text_msg_file_sections(
 
 int Read_text_msg_file_sections(
     istream &in,
-    vector<vector<char *> > &strings,   // Strings returned here
+    vector<vector<string> > &strings,   // Strings returned here
     const char *sections[],         // Section names
     int numsections
 ) {
@@ -237,28 +213,11 @@ int Read_text_msg_file_sections(
 void Write_msg_file_section(
     ostream &out,
     const char *section,
-    char **items,
-    int num_items
-) {
-	out << "%%section " << section << endl;
-	for (int i = 0; i < num_items; ++i)
-		if (items[i])
-			out << hex << "0x" << i << ':' << items[i] << endl;
-	out << "%%endsection " << section << endl;
-}
-
-/*
- *  Write one section.
- */
-
-void Write_msg_file_section(
-    ostream &out,
-    const char *section,
-    vector<char *> &items
+    vector<string> &items
 ) {
 	out << "%%section " << section << endl;
 	for (unsigned i = 0; i < items.size(); ++i)
-		if (items[i])
+		if (items[i].size())
 			out << hex << "0x" << i << ':' << items[i] << endl;
 	out << "%%endsection " << section << endl;
 }

files/msgfile.h
@@ -10,25 +10,19 @@
 using std::istream;
 using std::ostream;
 using std::vector;
+using std::string;
 class DataSource;
 
 int Read_text_msg_file(
     DataSource *in,
-    vector<char *> &strings,    // Strings returned here, each
+    vector<string> &strings,    // Strings returned here, each
     //   allocated on heap.
     const char *section = 0
 );
 int Read_text_msg_file(
     istream &in,
-    vector<char *> &strings,    // Strings returned here, each
+    vector<string> &strings,    // Strings returned here, each
     //   allocated on heap.
-    const char *section = 0
-);
-int Read_text_msg_file(
-    istream &in,
-    char ** &strings,       // Strings returned here, each
-    //   allocated on heap.
-    int &count,
     const char *section = 0
 );
 bool Search_text_msg_section(
@@ -37,26 +31,20 @@ bool Search_text_msg_section(
 );
 int Read_text_msg_file_sections(
     DataSource *in,
-    vector<vector<char *> > &strings,   // Strings returned here
+    vector<vector<string> > &strings,   // Strings returned here
     const char *sections[],         // Section names
     int numsections
 );
 int Read_text_msg_file_sections(
     istream &in,
-    vector<vector<char *> > &strings,   // Strings returned here
+    vector<vector<string> > &strings,   // Strings returned here
     const char *sections[],         // Section names
     int numsections
 );
 void Write_msg_file_section(
     ostream &out,
     const char *section,
-    char **items,
-    int num_items
-);
-void Write_msg_file_section(
-    ostream &out,
-    const char *section,
-    vector<char *> &items
+    vector<string> &items
 );
 
 #endif

commit 0a878e36f95553908e6c68327e4d753248f4f597

Fix text data file reading regression
Regression from 79c4f593a3b4b4b4c038cd97c16fc35b722c3217.

shapes/miscinf.cc
@@ -105,6 +105,7 @@ public:
 		src.ignore(off);
 		string ret;
 		getline(src, ret, '/');
+		src.unget();
 		return ret;
 	}
 };