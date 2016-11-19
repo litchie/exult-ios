98a866944523da70311cdd878f3af8c6bc3e973b
Marzo Sette Torres Junior <marzojr@yahoo.com>
2016-11-19 18:03:32 -0200

    Hooking in Travis CI.

---
+++ .travis/install-osx.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+MACPORTS_URL=https://distfiles.macports.org/MacPorts
+MACPORTS_PKG=MacPorts-2.3.3-10.9-Mavericks.pkg
+MACPORTS_PREFIX=/opt/local
+MACPORTS_PATH=$/tmp/$MACPORTS_PKG
+
+# Download and install MacPorts
+curl $MACPORTS_URL/$MACPORTS_PKG > $MACPORTS_PATH || exit 1
+sudo installer -pkg $MACPORTS_PATH -target / || exit 2
+export PATH=$MACPORTS_PREFIX/bin:$PATH
+
+# Just to be sure
+sudo port -v selfupdate
+sudo port upgrade outdated
+
+# Install actual dependencies
+sudo port install -q automake libtool pkgconfig libvorbis libpng zlib libglade2
+sudo port install -q libsdl +x11
+sudo port install -q libsdl2 +x11
+
---
+++ .travis/script.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+chmod a+x ./autogen.sh
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	export LIBOPTS=""
+	export EXTRA_TOOLS="--enable-gimp-plugin --enable-gnome-shp-thumbnailer --enable-compiler --enable-mods"
+else
+	export LIBOPTS="--enable-static-libraries --disable-alsa --disable-fluidsynth --disable-timidity-midi --with-macosx-static-lib-path=/opt/local/lib"
+	export EXTRA_TOOLS=""
+	export SDK=' -w -mmacosx-version-min=10.7 '
+	export MACOSX_DEPLOYMENT_TARGET=10.7
+	export CPPFLAGS='-I/opt/local/include '$SDK
+	export CFLAGS='-I/opt/local/include '$SDK
+	export CXXFLAGS='-I/opt/local/include '$SDK
+	export LDFLAGS='-L/opt/local/lib '$SDK
+	export PKG_CONFIG_PATH="/opt/local/lib/pkgconfig"
+	export PKG_CONFIG=/opt/local/bin/pkg-config
+	export LIBTOOLFLAGS="--silent"
+fi
+
+./autogen.sh && ./configure --with-cxx=`which $CXX` $LIBOPTS $DEBUG_OPT	\
+                            --disable-oggtest --disable-vorbistest	\
+                            --enable-exult-studio --enable-exult-studio-support	\
+                            --enable-mt32emu --enable-zip-support	\
+                            --enable-shared --enable-opengl --enable-midi-sfx	\
+                            $EXTRA_TOOLS && make
+
