785400f8747394ef59d114716c571ac368d342b3
DominusExult <domiman+github@gmail.com>
2017-11-25 02:37:18 +0100

    Updated SDL2 2.0.3 to SDL2 2.0.7 - stage 5: changes to SDL2 sources to do things different - src/events/SDL_mouse.c : make double_click_radius bigger to fix big fingers - src/video/uikit/SDL_uikitviewcontroller.m: Do not stop Text Input after hitting Return - src/video/uikit/SDL_uikitwindow.m: fix our borked SDLUIKitDelegate hijacking - this needs to be fixed on our side

--- ios/SDL2/src/video/uikit/SDL_uikitviewcontroller.m
+++ ios/SDL2/src/video/uikit/SDL_uikitviewcontroller.m
@@ -387,7 +387,8 @@ SDL_AppleTVControllerUIHintChanged(void *userdata, const char *name, const char 
 {
     SDL_SendKeyboardKey(SDL_PRESSED, SDL_SCANCODE_RETURN);
     SDL_SendKeyboardKey(SDL_RELEASED, SDL_SCANCODE_RETURN);
-    SDL_StopTextInput();
+    // SDL closes Text Input after Return but that is not desireable for multiline texts
+    //SDL_StopTextInput();
     return YES;
 }
 
