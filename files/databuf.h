litchie/exult-ios

commit e3abd804fdc82ba1f30b30da6a8fcc35b6cac768

Splitting DataSources into input and output.
At no point we read and write to the same data source.

Basically, reverting 82dfb06c and reapplying ffd481c7.

files/databuf.h
@@ -30,10 +30,10 @@
 
 typedef char *charptr;
 
-class DataSource {
+class IDataSource {
 public:
-	DataSource() {}
-	virtual ~DataSource() {}
+	IDataSource() {}
+	virtual ~IDataSource() {}
 
 	virtual uint32 peek() = 0;
 
@@ -45,20 +45,11 @@ public:
 	virtual void read(void *, size_t) = 0;
 	virtual void read(std::string&, size_t) = 0;
 
-	virtual void write1(uint32) = 0;
-	virtual void write2(uint16) = 0;
-	virtual void write2high(uint16) = 0;
-	virtual void write4(uint32) = 0;
-	virtual void write4high(uint32) = 0;
-	virtual void write(const void *, size_t) = 0;
-	virtual void write(const std::string &) = 0;
-
 	virtual void seek(size_t) = 0;
 	virtual void skip(std::streamoff) = 0;
 	virtual size_t getSize() = 0;
 	virtual size_t getPos() = 0;
 	virtual bool eof() = 0;
-	virtual void flush() { }
 	virtual bool good() {
 		return true;
 	}
@@ -77,40 +68,36 @@ public:
 	}
 };
 
-class StreamDataSource: public DataSource {
+class IStreamDataSource: public IDataSource {
 protected:
 	std::istream *in;
-	std::ostream *out;
 public:
-	StreamDataSource(std::istream *data_stream) : in(data_stream), out(0) {
+	IStreamDataSource(std::istream *data_stream) : in(data_stream) {
 	}
 
-	StreamDataSource(std::ostream *data_stream) : in(0), out(data_stream) {
-	}
+	virtual ~IStreamDataSource() {}
 
-	virtual ~StreamDataSource() {}
-
-	virtual uint32 peek()       {
+	virtual uint32 peek() {
 		return in->peek();
 	}
 
-	virtual uint32 read1()      {
+	virtual uint32 read1() {
 		return Read1(*in);
 	}
 
-	virtual uint16 read2()      {
+	virtual uint16 read2() {
 		return Read2(*in);
 	}
 
-	virtual uint16 read2high()  {
+	virtual uint16 read2high() {
 		return Read2high(*in);
 	}
 
-	virtual uint32 read4()      {
+	virtual uint32 read4() {
 		return Read4(*in);
 	}
 
-	virtual uint32 read4high()  {
+	virtual uint32 read4high() {
 		return Read4high(*in);
 	}
 
@@ -123,82 +110,38 @@ public:
 		in->read(&s[0], len);
 	}
 
-	virtual void write1(uint32 val)      {
-		Write1(*out, static_cast<uint16>(val));
-	}
-
-	virtual void write2(uint16 val)      {
-		Write2(*out, val);
-	}
-
-	virtual void write2high(uint16 val)  {
-		Write2high(*out, val);
-	}
-
-	virtual void write4(uint32 val)      {
-		Write4(*out, val);
-	}
-
-	virtual void write4high(uint32 val)  {
-		Write4high(*out, val);
-	}
-
-	virtual void write(const void *b, size_t len) {
-		out->write(static_cast<const char *>(b), len);
-	}
-
-	virtual void write(const std::string &s) {
-		out->write(&s[0], s.size());
-	}
-
 	virtual void seek(size_t pos) {
-		if (in) in->seekg(pos);
-		else out->seekp(pos);
+		in->seekg(pos);
 	}
 
 	virtual void skip(std::streamoff pos) {
-		if (in) in->seekg(pos, std::ios::cur);
-		else out->seekp(pos, std::ios::cur);
+		in->seekg(pos, std::ios::cur);
 	}
 
 	virtual size_t getSize() {
-		if (in) {
-			size_t pos = in->tellg();
-			in->seekg(0, std::ios::end);
-			size_t len = in->tellg();
-			in->seekg(pos);
-			return len;
-		} else {
-			size_t pos = out->tellp();
-			out->seekp(0, std::ios::end);
-			size_t len = out->tellp();
-			out->seekp(pos);
-			return len;
-		}
+		size_t pos = in->tellg();
+		in->seekg(0, std::ios::end);
+		size_t len = in->tellg();
+		in->seekg(pos);
+		return len;
 	}
 
 	virtual size_t getPos() {
-		return in ? in->tellg() : out->tellp();
+		return in->tellg();
 	}
 
 	virtual bool eof() {
-		return in->peek() == std::char_traits<char>::eof();
-	}
-	virtual void flush() {
-		if (out) out->flush();
+		return in->eof();
 	}
 	virtual bool good() {
-		return in ? in->good() : out->good();
+		return in->good();
 	}
 	virtual void clear_error() {
-		if (in)
-			in->clear();
-		else
-			out->clear();
+		in->clear();
 	}
 };
 
-class BufferDataSource: public DataSource {
+class IBufferDataSource: public IDataSource {
 protected:
 	/* const solely so that no-one accidentally modifies it.
 	    data is being passed 'non-const' anyway */
@@ -206,7 +149,7 @@ protected:
 	unsigned char *buf_ptr;
 	std::size_t size;
 public:
-	BufferDataSource(const void *data, size_t len) {
+	IBufferDataSource(const void *data, size_t len) {
 		// data can be NULL if len is also 0
 		assert(data != 0 || len == 0);
 		buf = static_cast<const unsigned char *>(data);
@@ -214,14 +157,7 @@ public:
 		size = len;
 	}
 
-	void load(void *data, size_t len) {
-		// data can be NULL if len is also 0
-		assert(data != 0 || len == 0);
-		buf = buf_ptr = static_cast<unsigned char *>(data);
-		size = len;
-	}
-
-	virtual ~BufferDataSource() {}
+	virtual ~IBufferDataSource() {}
 
 	virtual uint32 peek() {
 		unsigned char b0;
@@ -276,6 +212,174 @@ public:
 		s = std::string(reinterpret_cast<const char *>(buf_ptr), len);
 		buf_ptr += len;
 	}
+
+	virtual void seek(size_t pos) {
+		buf_ptr = const_cast<unsigned char *>(buf) + pos;
+	}
+
+	virtual void skip(std::streamoff pos) {
+		buf_ptr += pos;
+	}
+
+	virtual size_t getSize() {
+		return size;
+	}
+
+	virtual size_t getPos() {
+		return (buf_ptr - buf);
+	}
+
+	unsigned char *getPtr() {
+		return buf_ptr;
+	}
+
+	virtual bool eof() {
+		return (buf_ptr - buf) >= static_cast<std::ptrdiff_t>(size);
+	}
+};
+
+class IExultDataSource: public IBufferDataSource {
+public:
+	IExultDataSource(const File_spec &fname, int index)
+		: IBufferDataSource(0, 0) {
+		U7object obj(fname, index);
+		buf = buf_ptr = reinterpret_cast<unsigned char *>(obj.retrieve(size));
+	}
+
+	IExultDataSource(const File_spec &fname0, const File_spec &fname1, int index)
+		: IBufferDataSource(0, 0) {
+		U7multiobject obj(fname0, fname1, index);
+		buf = buf_ptr = reinterpret_cast<unsigned char *>(obj.retrieve(size));
+	}
+
+	IExultDataSource(const File_spec &fname0, const File_spec &fname1,
+	                const File_spec &fname2, int index)
+		: IBufferDataSource(0, 0) {
+		U7multiobject obj(fname0, fname1, fname2, index);
+		buf = buf_ptr = reinterpret_cast<unsigned char *>(obj.retrieve(size));
+	}
+
+	~IExultDataSource() {
+		delete [] const_cast<unsigned char *>(buf);
+	}
+};
+
+class ODataSource {
+public:
+	ODataSource() {}
+	virtual ~ODataSource() {}
+
+	virtual void write1(uint32) = 0;
+	virtual void write2(uint16) = 0;
+	virtual void write2high(uint16) = 0;
+	virtual void write4(uint32) = 0;
+	virtual void write4high(uint32) = 0;
+	virtual void write(const void *, size_t) = 0;
+	virtual void write(const std::string &) = 0;
+
+	virtual void seek(size_t) = 0;
+	virtual void skip(std::streamoff) = 0;
+	virtual size_t getSize() = 0;
+	virtual size_t getPos() = 0;
+	virtual void flush() { }
+	virtual bool good() {
+		return true;
+	}
+	virtual void clear_error() { }
+};
+
+class OStreamDataSource: public ODataSource {
+protected:
+	std::ostream *out;
+public:
+	OStreamDataSource(std::ostream *data_stream) : out(data_stream) {
+	}
+
+	virtual ~OStreamDataSource() {}
+
+	virtual void write1(uint32 val) {
+		Write1(*out, static_cast<uint16>(val));
+	}
+
+	virtual void write2(uint16 val) {
+		Write2(*out, val);
+	}
+
+	virtual void write2high(uint16 val) {
+		Write2high(*out, val);
+	}
+
+	virtual void write4(uint32 val) {
+		Write4(*out, val);
+	}
+
+	virtual void write4high(uint32 val) {
+		Write4high(*out, val);
+	}
+
+	virtual void write(const void *b, size_t len) {
+		out->write(static_cast<const char *>(b), len);
+	}
+
+	virtual void write(const std::string &s) {
+		out->write(&s[0], s.size());
+	}
+
+	virtual void seek(size_t pos) {
+		out->seekp(pos);
+	}
+
+	virtual void skip(std::streamoff pos) {
+		out->seekp(pos, std::ios::cur);
+	}
+
+	virtual size_t getSize() {
+		size_t pos = out->tellp();
+		out->seekp(0, std::ios::end);
+		size_t len = out->tellp();
+		out->seekp(pos);
+		return len;
+	}
+
+	virtual size_t getPos() {
+		return out->tellp();
+	}
+
+	virtual void flush() {
+		out->flush();
+	}
+	virtual bool good() {
+		return out->good();
+	}
+	virtual void clear_error() {
+		out->clear();
+	}
+};
+
+class OBufferDataSource: public ODataSource {
+protected:
+	/* const solely so that no-one accidentally modifies it.
+	    data is being passed 'non-const' anyway */
+	const unsigned char *buf;
+	unsigned char *buf_ptr;
+	std::size_t size;
+public:
+	OBufferDataSource(const void *data, size_t len) {
+		// data can be NULL if len is also 0
+		assert(data != 0 || len == 0);
+		buf = static_cast<const unsigned char *>(data);
+		buf_ptr = const_cast<unsigned char *>(buf);
+		size = len;
+	}
+
+	void load(void *data, size_t len) {
+		// data can be NULL if len is also 0
+		assert(data != 0 || len == 0);
+		buf = buf_ptr = static_cast<unsigned char *>(data);
+		size = len;
+	}
+
+	virtual ~OBufferDataSource() {}
 
 	virtual void write1(uint32 val) {
 		*buf_ptr++ = static_cast<uint8>(val & 0xff);
@@ -334,35 +438,30 @@ public:
 	unsigned char *getPtr() {
 		return buf_ptr;
 	}
-
-	virtual bool eof() {
-		return (buf_ptr - buf) >= static_cast<std::ptrdiff_t>(size);
-	}
-
 };
 
-class ExultDataSource: public BufferDataSource {
+class OExultDataSource: public OBufferDataSource {
 public:
-	ExultDataSource(const File_spec &fname, int index)
-		: BufferDataSource(0, 0) {
+	OExultDataSource(const File_spec &fname, int index)
+		: OBufferDataSource(0, 0) {
 		U7object obj(fname, index);
 		buf = buf_ptr = reinterpret_cast<unsigned char *>(obj.retrieve(size));
 	}
 
-	ExultDataSource(const File_spec &fname0, const File_spec &fname1, int index)
-		: BufferDataSource(0, 0) {
+	OExultDataSource(const File_spec &fname0, const File_spec &fname1, int index)
+		: OBufferDataSource(0, 0) {
 		U7multiobject obj(fname0, fname1, index);
 		buf = buf_ptr = reinterpret_cast<unsigned char *>(obj.retrieve(size));
 	}
 
-	ExultDataSource(const File_spec &fname0, const File_spec &fname1,
+	OExultDataSource(const File_spec &fname0, const File_spec &fname1,
 	                const File_spec &fname2, int index)
-		: BufferDataSource(0, 0) {
+		: OBufferDataSource(0, 0) {
 		U7multiobject obj(fname0, fname1, fname2, index);
 		buf = buf_ptr = reinterpret_cast<unsigned char *>(obj.retrieve(size));
 	}
 
-	~ExultDataSource() {
+	~OExultDataSource() {
 		delete [] const_cast<unsigned char *>(buf);
 	}
 };

shapes/vgafile.cc
@@ -333,7 +333,7 @@ Shape_frame::Shape_frame(
  */
 
 unsigned int Shape_frame::read(
-    DataSource *shapes,     // Shapes data source to read.
+    IDataSource *shapes,     // Shapes data source to read.
     uint32 shapeoff,        // Offset of shape in file.
     uint32 shapelen,        // Length expected for detecting RLE.
     int frnum           // Frame #.
@@ -387,7 +387,7 @@ unsigned int Shape_frame::read(
  */
 
 void Shape_frame::get_rle_shape(
-    DataSource *shapes,     // Shapes data source to read.
+    IDataSource *shapes,     // Shapes data source to read.
     long filepos,           // Position in file.
     long len            // Length of entire frame data.
 ) {
@@ -718,7 +718,7 @@ void Shape_frame::set_offset(
  */
 
 Shape_frame *Shape::reflect(
-    vector<pair<DataSource *, bool> > const &shapes,    // shapes data source to read.
+    vector<pair<IDataSource *, bool> > const &shapes,    // shapes data source to read.
     int shapenum,           // Shape #.
     int framenum,           // Frame # without the 'reflect' bit.
     vector<int> const &counts
@@ -804,13 +804,13 @@ inline void Shape::create_frames_list(
  */
 
 Shape_frame *Shape::read(
-    vector<pair<DataSource *, bool> > const &shapes, // Shapes data source to read.
+    vector<pair<IDataSource *, bool> > const &shapes, // Shapes data source to read.
     int shapenum,           // Shape #.
     int framenum,           // Frame # within shape.
     vector<int> const &counts,      // Number of shapes in files.
     int src
 ) {
-	DataSource *shp = 0;
+	IDataSource *shp = 0;
 	// Figure offset in "shapes.vga".
 	uint32 shapeoff = 0x80 + shapenum * 8;
 	uint32 shapelen = 0;
@@ -818,11 +818,11 @@ Shape_frame *Shape::read(
 	// Check backwards for the shape file to use.
 	int i = counts.size();
 	if (src < 0) {
-		vector<pair<DataSource *, bool> >::const_reverse_iterator it = shapes.rbegin();
+		vector<pair<IDataSource *, bool> >::const_reverse_iterator it = shapes.rbegin();
 		for (; it != shapes.rend(); ++it) {
 			i--;
 			if (shapenum < counts[i]) {
-				DataSource *ds = (*it).first;
+				IDataSource *ds = (*it).first;
 				ds->seek(shapeoff);
 				// Get location, length.
 				int s = ds->read4();
@@ -837,7 +837,7 @@ Shape_frame *Shape::read(
 			}
 		}
 	} else if (shapenum < counts[src]) {
-		DataSource *ds = shapes[src].first;
+		IDataSource *ds = shapes[src].first;
 		ds->seek(shapeoff);
 		// Get location, length.
 		int s = ds->read4();
@@ -1006,7 +1006,7 @@ void Shape::take(
  */
 
 void Shape::load(
-    DataSource *shape_source        // datasource.
+    IDataSource *shape_source        // datasource.
 ) {
 	reset();
 	Shape_frame *frame = new Shape_frame();
@@ -1101,7 +1101,7 @@ void Shape_file::load(
 ) {
 	ifstream file;
 	U7open(file, nm);
-	StreamDataSource shape_source(&file);
+	IStreamDataSource shape_source(&file);
 	Shape::load(&shape_source);
 }
 
@@ -1110,7 +1110,7 @@ void Shape_file::load(
  */
 
 Shape_file::Shape_file(
-    DataSource *shape_source        // datasource.
+    IDataSource *shape_source        // datasource.
 ) {
 	Shape::load(shape_source);
 }
@@ -1126,7 +1126,7 @@ int Shape_file::get_size() {
 }
 
 // NOTE: Only works on shapes other than the special 8x8 tile-shapes
-void Shape_file::save(DataSource *shape_source) {
+void Shape_file::save(ODataSource *shape_source) {
 	int *offsets = new int[num_frames];
 	int size;
 	offsets[0] = 4 + num_frames * 4;
@@ -1187,21 +1187,21 @@ bool Vga_file::load(
 	return load(src, resetimports);
 }
 
-DataSource *Vga_file::U7load(
+IDataSource *Vga_file::U7load(
     pair<string, int> const &resource,
     vector<ifstream *> &fs,
     vector<char *> &bs,
-    vector<pair<DataSource *, bool> > &shps
+    vector<pair<IDataSource *, bool> > &shps
 ) {
-	DataSource *source = 0;
+	IDataSource *source = 0;
 	if (resource.second < 0) {
 		// It is a file.
 		if (U7exists(resource.first)) {
 			ifstream *file = new ifstream();
 			U7open(*file, resource.first.c_str());
-			source = new StreamDataSource(file);
+			source = new IStreamDataSource(file);
 			fs.push_back(file);
-			shps.push_back(pair<DataSource *, bool>(source,
+			shps.push_back(pair<IDataSource *, bool>(source,
 			                                        !resource.first.compare(0, 7, "<PATCH>")));
 		}
 	} else {
@@ -1214,9 +1214,9 @@ DataSource *Vga_file::U7load(
 			delete [] buf;
 			return 0;
 		}
-		source = new BufferDataSource(buf, len);
+		source = new IBufferDataSource(buf, len);
 		bs.push_back(buf);
-		shps.push_back(pair<DataSource *, bool>(source, false));
+		shps.push_back(pair<IDataSource *, bool>(source, false));
 	}
 	return source;
 }
@@ -1238,7 +1238,7 @@ bool Vga_file::load(
 		is_good = false;
 	for (vector<pair<string, int> >::const_iterator it = sources.begin();
 	        it != sources.end(); ++it) {
-		DataSource *source = U7load(*it, files, buffers, shape_sources);
+		IDataSource *source = U7load(*it, files, buffers, shape_sources);
 		if (source) {
 			flex = Flex::is_flex(source);
 			if (flex) {
@@ -1285,7 +1285,7 @@ bool Vga_file::import_shapes(
     vector<pair<int, int> > const &imports
 ) {
 	reset_imports();
-	DataSource *ds =
+	IDataSource *ds =
 	    U7load(source, imported_files, imported_buffers, imported_sources);
 	if (ds) {
 		ds->seek(0x54); // Get # of shapes.
@@ -1320,7 +1320,7 @@ bool Vga_file::import_shapes(
 void Vga_file::reset() {
 	delete [] shapes;
 
-	for (vector<pair<DataSource *, bool> >::iterator it = shape_sources.begin();
+	for (vector<pair<IDataSource *, bool> >::iterator it = shape_sources.begin();
 	        it != shape_sources.end(); ++it)
 		delete it->first;
 	shape_sources.clear();
@@ -1350,7 +1350,7 @@ void Vga_file::reset_imports() {
 		delete *it;
 	imported_shapes.clear();
 
-	for (vector<pair<DataSource *, bool> >::iterator it = imported_sources.begin();
+	for (vector<pair<IDataSource *, bool> >::iterator it = imported_sources.begin();
 	        it != imported_sources.end(); ++it)
 		delete it->first;
 	imported_sources.clear();

commit 1e9e00c1e2853a21183724031886f765b8bae7f3

Fixing issues caused by e3abd804

files/databuf.h
@@ -131,7 +131,12 @@ public:
 	}
 
 	virtual bool eof() {
-		return in->eof();
+		in->get();
+		bool ret = in->eof();
+		if (!ret) {
+			in->unget();
+		}
+		return ret;
 	}
 	virtual bool good() {
 		return in->good();

shapes/vgafile.cc
@@ -1010,14 +1010,15 @@ void Shape::load(
 ) {
 	reset();
 	Shape_frame *frame = new Shape_frame();
+	size_t location = shape_source->getPos();
 	uint32 shapelen = shape_source->read4();
 	// Read frame 0 & get frame count.
-	create_frames_list(frame->read(shape_source, 0L, shapelen, 0));
+	create_frames_list(frame->read(shape_source, location, shapelen, 0));
 	store_frame(frame, 0);
 	// Get the rest.
 	for (size_t i = 1; i < num_frames; i++) {
 		frame = new Shape_frame();
-		frame->read(shape_source, 0L, shapelen, i);
+		frame->read(shape_source, location, shapelen, i);
 		store_frame(frame, i);
 	}
 }